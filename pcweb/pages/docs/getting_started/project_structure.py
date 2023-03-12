0840004e23f04993264bf5a5e0eca4c0c0874596
Nikhil Rao <nikhil@pynecone.io>
2023-03-12 14:28:58 -0700

    Add docs about var operations (#33)

--- pcweb/pages/docs/getting_started/project_structure.py
+++ pcweb/pages/docs/getting_started/project_structure.py
@@ -81,16 +81,6 @@ $ pc init""",
         doccode(
             """pc.image(src="image.png")""",
         ),
-        doctext(
-            pc.alert(
-                pc.alert_icon(),
-                pc.alert_title(
-                    "You will have to restart your app to see changes to "
-                    " the assets directory.",
-                ),
-                status="warning",
-            ),
-        ),
         subheader("Main Project"),
         doctext(
             "Initializing your project creates a directory with the same name as your app. ",
