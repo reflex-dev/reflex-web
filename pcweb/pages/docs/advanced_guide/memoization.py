05df17885e99d5861f9128242ff958895686a669
Nikhil Rao <nikhil@pynecone.io>
2023-04-03 21:00:11 -0700

    one more

--- pcweb/pages/docs/advanced_guide/memoization.py
+++ pcweb/pages/docs/advanced_guide/memoization.py
@@ -19,10 +19,10 @@ def memoization():
         docheader("Memoization", first=True),
         doctext(
             "By default, Pynecone will re-render the entire page every time the state changes. "
-            "This is fine for small app's, but can be slow for larger apps. "
+            "This is fine for small apps, but can be slow for larger apps. "
         ),
         doctext(
-            "You can prevent unnecessary rendering by memoizing components, which can greatly improve your apps performance. ",
+            "You can prevent unnecessary rendering by memoizing components, which can greatly improve your app's performance. ",
             "Just add the ",
             pc.code("@pc.memo"),
             " decorator to a component function to make it memoized, ",
