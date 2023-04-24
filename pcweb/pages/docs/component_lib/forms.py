f3d08340d950487adda60dcf8a3b503bd25c7ba8
Elijah Ahianyo <elijahahianyo@gmail.com>
2023-04-24 20:02:26 +0000

    added example for formerrormessage (#56)

--- pcweb/pages/docs/component_lib/forms.py
+++ pcweb/pages/docs/component_lib/forms.py
@@ -236,12 +236,47 @@ code15 = """pc.form_control(
 """
 
 
+form_error_state = """class FormErrorState(State):
+    name: str
+
+    @pc.var
+    def is_error(self) -> bool:
+         return len(self.name) <= 3
+"""
+exec(form_error_state)
+
+form_state_example = """pc.vstack(
+        pc.form_control(
+            pc.input(placeholder="name", on_change=FormErrorState.set_name),
+            pc.cond(
+                FormErrorState.is_error,
+                pc.form_error_message("Name should be more than four characters"),
+                pc.form_helper_text("Enter name"),
+            ),
+            is_invalid=FormErrorState.is_error,
+            is_required=True,
+
+        )
+    )
+
+"""
+
+
 def render_formcontrol():
     return pc.vstack(
         doctext(
             "Provides context such as filled/focused/error/required for form inputs."
         ),
         docdemo(code15),
+        doctext(
+            "The example below shows a form error when then name length is 3 or less."
+        ),
+        docdemo(
+            form_state_example,
+            state=form_error_state,
+            comp=eval(form_state_example),
+            context=True,
+        ),
         align_items="start",
     )
 
@@ -283,13 +318,7 @@ clear_input_example = """pc.vstack(
     pc.button("Clear", on_click=ClearInputState.clear_text),
 )
 """
-clear_input_state = """
-class ClearInputState(State):
-    text: str
 
-    def clear_text(self):
-        self.text = ""
-"""
 exec(clear_input_state)
 key_press_state = """
 class KeyPressInputState(State):
