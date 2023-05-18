30183d8e409c6376bf9913832f19b55a41cce67b
Curtis Turner <cjturner714@gmail.com>
2023-05-18 09:33:06 -0700

    Update middleware.py
    Update docs example code for `postprocess` to align with the latest release `0.1.31` currently so users avoid any errors.

--- pcweb/pages/docs/advanced_guide/middleware.py
+++ pcweb/pages/docs/advanced_guide/middleware.py
@@ -24,7 +24,7 @@ def middleware():
     def preprocess(self, app, state, event):
         print(f"Event {event}")
 
-    def postprocess(self, app, state, event, delta):
+    def postprocess(self, app, state, event, update):
         print(f"Delta {delta}")"""
         ),
         doctext(
