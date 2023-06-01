e3f11f3ec0cf4b7c19de83857f2746b04ba8901e
Lendemor <thomas.brandeho@gmail.com>
2023-06-01 14:36:59 +0200

    fix import

--- pcweb/pages/docs/advanced_guide/__init__.py
+++ pcweb/pages/docs/advanced_guide/__init__.py
@@ -1,6 +1,6 @@
 from .custom_vars import custom_vars
 from .memoization import memoization
-from .middleware import use_middleware
+from .use_middleware import use_middleware
 from .wrapping_react import wrapping_react
 from .api_routes import api_routes
 from .telemetry import telemetry
