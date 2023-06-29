23922935b21806e32f6afccd8fe6d73c31c371b3
Lendemor <thomas.brandeho@gmail.com>
2023-06-29 20:05:32 +0200

    comment out cookie and add warning for local storage

--- pcweb/pages/docs/api_reference/browser.py
+++ pcweb/pages/docs/api_reference/browser.py
@@ -8,6 +8,7 @@ from pcweb.templates.docpage import (
     doctext,
     subheader,
     doclink,
+    docalert,
 )
 
 code_state_gc = """
@@ -27,11 +28,11 @@ rx.button(CookieState.cookie_val, on_click=CookieState.set_cookie)
 """
 
 code_gls = """
-rx.button("Copy value of 'key' in local storage to clipboard", on_click=rx.set_clipboard(rx.get_local_storage("key")))
+rx.button("Copy Local Storage to clipboard", on_click=rx.set_clipboard(rx.get_local_storage("key")))
 """
 code_sls = """
 rx.button(
-    "Set 'key' in local storage", on_click=rx.set_local_storage("key", "value1")
+    "Set Local Storage", on_click=rx.set_local_storage("key", "value1")
 )
 """
 
@@ -40,23 +41,23 @@ rx.button(
 def browser():
     return rx.box(
         docheader("Browser Storage"),
-        subheader("Cookies"),
-        doctext(
-            "If you want to manipulate cookies in your Reflex app, we provide some getters and setters to ",
-            "manipulate them.",
-        ),
-        doctext(
-            "To read the value of a cookie, inside an event handler, you can use",
-            rx.code("self.get_cookies()"),
-        ),
-        # docdemo(code_gc, state=code_state_gc2, comp=eval(code_gc)),
-        doctext(
-            "To write a cookie, you can use the ",
-            doclink("special event", href="/docs/api-reference/special-event"),
-            " ",
-            rx.code("set_cookie(key , value)"),
-        ),
-        docdemo(code_sc, state=code_state_gc, comp=eval(code_sc)),
+        # subheader("Cookies"),
+        # doctext(
+        #     "If you want to manipulate cookies in your Reflex app, we provide some getters and setters to ",
+        #     "manipulate them.",
+        # ),
+        # doctext(
+        #     "To read the value of a cookie, inside an event handler, you can use",
+        #     rx.code("self.get_cookies()"),
+        # ),
+        # # docdemo(code_gc, state=code_state_gc2, comp=eval(code_gc)),
+        # doctext(
+        #     "To write a cookie, you can use the ",
+        #     doclink("special event", href="/docs/api-reference/special-event"),
+        #     " ",
+        #     rx.code("set_cookie(key , value)"),
+        # ),
+        # docdemo(code_sc, state=code_state_gc, comp=eval(code_sc)),
         subheader("Local Storage"),
         doctext(
             "Aside from cookies, if for some reason you need to use the Local Storage on your Reflex app, you can do so with:"
@@ -72,4 +73,11 @@ def browser():
             rx.code("rx.set_local_storage(key,value)"),
         ),
         docdemo(code_sls),
+        docalert(
+            rx.text(
+                rx.code("get_local_storage()"),
+                " cannot be used for direct rendering",
+            ),
+            status="warning",
+        ),
     )
