e7d970e0a5c6a34b82651259bcd1085c5c844fc6
Nikhil Rao <nikhil@pynecone.io>
2023-08-02 02:45:47 -0700

    Remove key

--- pcweb/pages/docs/tutorial/adding_state.py
+++ pcweb/pages/docs/tutorial/adding_state.py
@@ -11,7 +11,7 @@ from pcweb.templates.docpage import (
     subheader,
 )
 import openai
-openai.api_key = "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"
+openai.api_key = "YOUR_OPENAI_KEY"
 
 
 class ChatappState(State):
@@ -280,79 +280,5 @@ def adding_state():
         docdemobox(code_out3),
         doctext(
             "In the next section, we will finish our chatbot by adding AI!",
-        ),
-        doccode(
-            '''import reflex as rx
-import openai
-from .styles import input_style, message_style, button_style
-
-# Replace 'YOUR_API_KEY' with your actual API key
-openai.api_key = 'YOUR_API_KEY'
-
-class State(rx.State):
-    """The app state."""
-
-    question: str = "What is the meaning of life?"
-    
-    answer: str = "42"
-
-    async def process_question(self):
-        self.answer = ""
-        yield
-
-        session = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                { "role": "user", "content": self.question}
-            ],
-            stop=None,
-            temperature=0.7,
-            stream=True,
-        )
-
-        for item in session:
-            if hasattr(item.choices[0].delta, "content"):        
-                self.answer += item.choices[0].delta.content
-                yield
-        '''
-        ),
-        doctext(
-            """
-            Now we can hook up the variable and event handlers to our UI.
-            """,
-        ),
-        doccode(
-            '''def qa() -> rx.Component:
-    return rx.box(
-        rx.box(
-            rx.text(
-                State.question,
-                bg="#F5EFFE",
-                style=message_style,
-            ),
-            text_align="right",
-            margin_top="1em",
-        ),
-        rx.box(
-            rx.text(
-                State.answer,
-                bg="#DEEAFD",
-                style=message_style,
-            ),
-            text_align="left",
-            padding_top="1em",
-        ),
-        width="100%",
-    )
-
-
-def action_bar() -> rx.Component:
-    """The action bar to send a new message."""
-    return rx.hstack(
-        rx.input(placeholder="Ask a question...", style=input_style, on_blur=State.set_question),
-        rx.button("Ask", on_click=State.process_question, style=button_style),
-        width="100%",
-    )
-            '''
         ),
     )
