9a357995a0da50690261d8c67a597f4eb3c72093
Alek Petuskey <alekpetuskey@aleks-mbp.lan>
2023-08-01 22:37:25 -0700

    take out extra

--- pcweb/components/navbar.py
+++ pcweb/components/navbar.py
@@ -237,7 +237,7 @@ def navbar(sidebar: rx.Component = None) -> rx.Component:
                 rx.hstack(
                     rx.center(
                         rx.box(
-                            "✨ Announcing our seed round led by Lux Capital.",
+                            "✨ We're rebranding to Reflex!",
                             " Read more about it on our ",
                             rx.link(
                                 "blog",
