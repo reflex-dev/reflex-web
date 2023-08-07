370eb99bcb97de4fd9e3e775aebdd1438756dd0b
Alek Petuskey <alekpetuskey@aleks-mbp.lan>
2023-08-07 15:18:21 -0700

    General improvement and fixing some bug reports

--- pcweb/constants.py
+++ pcweb/constants.py
@@ -12,7 +12,7 @@ LINKEDIN_URL = "https://www.linkedin.com/company/reflex-dev"
 YC_URL = "https://www.ycombinator.com/companies/reflex"
 ROADMAP_URL = "https://reflex-dev.notion.site/d1b60926ced14914bdbb42547ac84e50?v=723e0afc57294e40961d177aa691ee37"
 CONTRIBUTOR_PROGRAM_URL = "https://reflex-dev.notion.site/2107ab2bc166497db951b8d742748284?v=f0eaff78fa984b5ab15d204af58907d7"
-
+REPORT_A_BUG_URL = "https://github.com/reflex-dev/reflex/issues/new?assignees=&labels=&projects=&template=bug_report.md&title="
 # Install urls.
 BUN_URL = "https://bun.sh"
 NEXTJS_URL = "https://nextjs.org"
@@ -23,3 +23,8 @@ VENV_URL = "https://docs.python.org/3/library/venv.html"
 VIRTUALENV_URL = "https://virtualenv.pypa.io/en/latest/"
 CONDA_URL = "https://docs.conda.io/en/latest/"
 FASTAPI_URL = "https://fastapi.tiangolo.com"
+
+# Stats
+GITHUB_STARS = 11000
+MONTHLY_USERS = 3000
+DISCORD = 1600
