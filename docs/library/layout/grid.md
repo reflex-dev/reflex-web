6284a3deb5bd1f3517c64396797dd38ea3bb7c49
Tom Gotsman <tomgotsman@toms-mbp.lan>
2023-09-21 11:55:31 -0700

    update docs grid component in flexdown

---
+++ docs/library/layout/grid.md
@@ -0,0 +1,42 @@
+---
+import reflex as rx
+from pcweb.templates.docpage import docdemo
+
+grid_simple_example = """rx.grid(
+    rx.grid_item(row_span=1, col_span=1, bg="lightgreen"),
+    rx.grid_item(row_span=1, col_span=1, bg="lightblue"),
+    rx.grid_item(row_span=1, col_span=1, bg="purple"),
+    rx.grid_item(row_span=1, col_span=1, bg="orange"),
+    rx.grid_item(row_span=1, col_span=1, bg="yellow"),
+    template_columns="repeat(5, 1fr)",
+    h="10em",
+    width="100%",
+    gap=4,
+)"""
+
+grid_complex_example = """rx.grid(
+    rx.grid_item(row_span=2, col_span=1, bg="lightblue"),
+    rx.grid_item(col_span=2, bg="lightgreen"),
+    rx.grid_item(col_span=2, bg="yellow"),
+    rx.grid_item(col_span=4, bg="orange"),
+    template_rows="repeat(2, 1fr)",
+    template_columns="repeat(5, 1fr)",
+    h="200px",
+    width="100%",
+    gap=4,
+)"""
+
+---
+
+A primitive useful for grid layouts. Grid is Box with display, grid and it comes with helpful style shorthand. It renders a div element.
+
+```reflex
+docdemo(grid_simple_example)
+```
+
+In some layouts, you may need certain grid items to span specific amount of columns or rows instead of an even distribution. To achieve this, you need to pass the col_span prop to the GridItem component to span across columns and also pass the row_span component to span across rows. You also need to specify the template_columns and template_rows.
+
+```reflex
+docdemo(grid_complex_example)
+```
+
--- pcweb/pages/docs/component_lib/layout.py
+++ pcweb/pages/docs/component_lib/layout.py
@@ -111,7 +111,6 @@ def render_container():
     )
 
 
-
 def render_flex():
     # Get the file.
     front_matter, output = flexdown.read("docs/library/layout/flex.md")
@@ -120,44 +119,13 @@ def render_flex():
     )
 
 
-code58 = """rx.grid(
-    rx.grid_item(row_span=1, col_span=1, bg="lightgreen"),
-    rx.grid_item(row_span=1, col_span=1, bg="lightblue"),
-    rx.grid_item(row_span=1, col_span=1, bg="purple"),
-    rx.grid_item(row_span=1, col_span=1, bg="orange"),
-    rx.grid_item(row_span=1, col_span=1, bg="yellow"),
-    template_columns="repeat(5, 1fr)",
-    h="10em",
-    width="100%",
-    gap=4,
-)
-"""
-code59 = """rx.grid(
-    rx.grid_item(row_span=2, col_span=1, bg="lightblue"),
-    rx.grid_item(col_span=2, bg="lightgreen"),
-    rx.grid_item(col_span=2, bg="yellow"),
-    rx.grid_item(col_span=4, bg="orange"),
-    template_rows="repeat(2, 1fr)",
-    template_columns="repeat(5, 1fr)",
-    h="200px",
-    width="100%",
-    gap=4,
-)
-"""
-
-
 def render_grid():
-    return rx.vstack(
-        doctext(
-            "A primitive useful for grid layouts. Grid is Box with display, grid and it comes with helpful style shorthand. It renders a div element."
-        ),
-        docdemo(code58),
-        doctext(
-            "In some layouts, you may need certain grid items to span specific amount of columns or rows instead of an even distribution. To achieve this, you need to pass the col_span prop to the GridItem component to span across columns and also pass the row_span component to span across rows. You also need to specify the template_columns and template_rows."
-        ),
-        docdemo(code59),
-        align_items="start",
+    # Get the file.
+    front_matter, output = flexdown.read("docs/library/layout/grid.md")
+    return rx.box(
+        *output,
     )
+
 
 
 code60 = """rx.responsive_grid(
