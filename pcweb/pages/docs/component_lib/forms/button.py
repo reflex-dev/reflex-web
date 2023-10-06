0350c205d4c95e4ff73e29fb9bdb17622aa4ed0d
Nikhil Rao <nikhil@reflex.dev>
2023-10-05 17:48:35 -0700

    Remove pcweb flexdown

--- pcweb/pages/docs/component_lib/forms/button.py
+++ pcweb/pages/docs/component_lib/forms/button.py
@@ -1,9 +1,6 @@
-import reflex as rx
-from pcweb import flexdown
+from pcweb.flexdown import component_map
+import flexdown
 
 
 def render_button():
-    _, output = flexdown.read("docs/library/forms/button.md")
-    return rx.box(
-        *output,
-    )
+    return flexdown.render_file("docs/library/forms/button.md", component_map=component_map)
