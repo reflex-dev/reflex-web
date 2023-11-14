05d50f117527803f40254bb0eab6f775f8203ffb
Nikhil Rao <nikhil@reflex.dev>
2023-11-14 15:18:09 -0800

    Fix tsclient import

--- pcweb/tsclient.py
+++ pcweb/tsclient.py
@@ -2,12 +2,15 @@
 import typesense
 import os
 
-client = typesense.Client({
-  'api_key': os.getenv("TYPESENSE_API_KEY"),
-  'nodes': [{
-    'host': '6xtoqsb1a4ip9u8gp-1.a1.typesense.net',
-    'port': '443',
-    'protocol': 'https'
-  }],
-  'connection_timeout_seconds': 2
-})
+try:
+  client = typesense.Client({
+    'api_key': os.getenv("TYPESENSE_API_KEY"),
+    'nodes': [{
+      'host': '6xtoqsb1a4ip9u8gp-1.a1.typesense.net',
+      'port': '443',
+      'protocol': 'https'
+    }],
+    'connection_timeout_seconds': 2
+  })
+except:
+  client = None
