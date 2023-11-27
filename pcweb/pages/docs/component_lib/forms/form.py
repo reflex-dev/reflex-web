be2abea51a2873bd847e4f8032865a2fc716bb34
Masen Furer <m_github@0x26.net>
2023-11-27 15:14:49 -0800

    Update form docs; include dynamic form example (#325)

--- pcweb/pages/docs/component_lib/forms/form.py
+++ pcweb/pages/docs/component_lib/forms/form.py
@@ -1,13 +1,4 @@
 from pcweb import flexdown
-from pcweb.base_state import State
-
-
-class FormState(State):
-    form_data: dict = {}
-
-    def handle_submit(self, form_data: dict):
-        """Handle the form submit."""
-        self.form_data = form_data
 
 
 def render_form():
