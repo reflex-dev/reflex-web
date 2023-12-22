940f01b92e99c13c50490e93250331489ea25033
Tom Gotsman <tomgotsman@Toms-MacBook-Pro.local>
2023-12-22 12:31:18 -0800

    update the radix text md file

--- pcweb/pages/docs/__init__.py
+++ pcweb/pages/docs/__init__.py
@@ -29,6 +29,5 @@ from .conditional_rendering import conditional_rendering
 from .rendering_iterables import rendering_iterables
 from .component_lib.data_display.radix_badge import radix_badge
 from .radix_dialog import radix_dialog
-from .radix_text import radix_text
 
 doc_routes = [r for r in locals().values() if isinstance(r, Route)]
--- pcweb/pages/docs/radix_text.py
+++
@@ -1,8 +0,0 @@
-from pcweb import flexdown
-
-from pcweb.templates.docpage import docpage
-
-
-@docpage()
-def radix_text():
-    return flexdown.render_file("docs/radix_text.md")
