129c8e9bdad54181e89402100edbf479735eea97
Nikhil Rao <nikhil@reflex.dev>
2024-01-05 16:20:35 -0800

    Bring back tabs

--- pcweb/pages/docs/__init__.py
+++ pcweb/pages/docs/__init__.py
@@ -12,18 +12,24 @@ from .api_routes import *
 doc_routes = [r for r in locals().values() if isinstance(r, Route)]
 
 from types import SimpleNamespace
-from pcweb.templates.docpage import docpage
+from pcweb.templates.docpage import docpage, component_docpage
 import flexdown
 
 flexdown_docs = flexdown.utils.get_flexdown_files("docs/")
 for doc in flexdown_docs:
-    if doc.startswith("docs/library"):
+    if doc.startswith("docs/library/chakra"):
+        continue
+    if doc.endswith("-style.md"):
         continue
 
     # Get the docpage component.
     route = f"/{doc.replace('.md', '')}"
     title = rx.utils.format.to_snake_case(doc.rsplit("/", 1)[1].replace(".md", ""))
-    comp = docpage(set_path=route, t=rx.utils.format.to_title_case(title))(lambda doc=doc: flexdown.render_file(doc))
+    if doc.startswith("docs/library"):
+        comp = component_docpage(set_path=route.strip("/"), t=rx.utils.format.to_title_case(title))
+    else:
+        comp = docpage(set_path=route, t=rx.utils.format.to_title_case(title))(lambda doc=doc: flexdown.render_file(doc))
+
 
     # Get the namespace.
     namespace = rx.utils.format.to_snake_case(doc.split("/")[1])
