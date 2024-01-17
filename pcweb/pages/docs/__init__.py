914e9e1150669f695c377d49577f33b7f92ae729
Nikhil Rao <nikhil@reflex.dev>
2024-01-17 11:32:29 -0800

    Fix paths

--- pcweb/pages/docs/__init__.py
+++ pcweb/pages/docs/__init__.py
@@ -28,59 +28,50 @@ from reflex.components.radix.themes.base import RadixThemesComponent
 import os
 from pcweb.route import Route
 
-for doc in sorted(flexdown_docs):
-    if doc.endswith("-style.md"):
-        continue
 
+def should_skip_compile(doc, prefix=""):
     # Check if the doc has been compiled already.
-    compiled_output = f".web/pages/{doc.replace('.md', '.js')}"
+    compiled_output = f".web/pages/{doc.replace('.md', '.js').replace('library/', f'library/{prefix}')}"
     # Get the timestamp of the compiled file.
     compiled_time = (
         os.path.getmtime(compiled_output) if os.path.exists(compiled_output) else 0
     )
     # Get the timestamp of the source file.
     source_time = os.path.getmtime(doc)
+    return compiled_time > source_time
+
+
+for doc in sorted(flexdown_docs):
+    if doc.endswith("-style.md"):
+        continue
+
     # Get the docpage component.
     route = f"/{doc.replace('.md', '')}"
     title = rx.utils.format.to_snake_case(doc.rsplit("/", 1)[1].replace(".md", ""))
     category = doc.split("/")[-2].title()
 
     # Skip the doc if it has already been compiled.
-    skip = False
-    if compiled_time > source_time:
-        skip = True
-        # # Get the namespace.
-        # namespace = rx.utils.format.to_snake_case(doc.split("/")[1])
-
-        # # Create a namespace if it doesn't exist.
-        # if namespace not in locals():
-        #     locals()[namespace] = SimpleNamespace()
-
-        # # Add the component to the namespace.
-        # setattr(locals()[namespace], title, Route(path=route, title="", component = lambda: rx.markdown("Hi")))
-        # continue
-
-    d = flexdown.parse_file(doc)
     if doc.startswith("docs/library/chakra"):
-        if skip:
+        if should_skip_compile(doc):
             continue
+        d = flexdown.parse_file(doc)
         clist = [title, *[eval(c) for c in d.metadata["components"]]]
         component_list[category].append(clist)
         comp = multi_docs(path=route, comp=d, component_list=clist, title=title)
     elif doc.startswith("docs/library"):
-        if skip:
-            continue
+        d = flexdown.parse_file(doc)
         clist = [title, *[eval(c) for c in d.metadata["components"]]]
         if issubclass(clist[1], RadixThemesComponent):
             radix_components[category].append(clist)
-            # route = route.replace("library/", "library/radix/")
+            route = route.replace("library/", "library/radix/")
+            prefix = "radix/"
         else:
             component_list[category].append(clist)
+            prefix = ""
+        if should_skip_compile(doc, prefix=prefix):
+            continue
         comp = multi_docs(path=route, comp=d, component_list=clist, title=title)
     else:
-        comp = docpage(set_path=route, t=to_title_case(title))(
-            lambda d=d, doc=doc: xd.render(d, doc)
-        )
         # Get the namespace.
         namespace = rx.utils.format.to_snake_case(doc.split("/")[1])
 
@@ -89,10 +80,15 @@ for doc in sorted(flexdown_docs):
             locals()[namespace] = SimpleNamespace()
 
         # Add the component to the namespace.
-        setattr(locals()[namespace], title, comp)
+        setattr(locals()[namespace], title, Route(path=route, title="", component=lambda: ""))
 
-        if skip:
+        if should_skip_compile(doc):
             continue
+
+        d = flexdown.parse_file(doc)
+        comp = docpage(set_path=route, t=to_title_case(title))(
+            lambda d=d, doc=doc: xd.render(d, doc)
+        )
 
     # Add the route to the list of routes.
     doc_routes.append(comp)
