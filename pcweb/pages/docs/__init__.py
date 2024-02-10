9802142a1d4a350482681abc3a52e45ccba7f6de
Nikhil Rao <nikhil@reflex.dev>
2024-02-10 10:40:09 +0700

    Move Chakra components to "other" section (#444)

--- pcweb/pages/docs/__init__.py
+++ pcweb/pages/docs/__init__.py
@@ -87,7 +87,6 @@ flexdown_docs = [
 ]
 
 chakra_components = defaultdict(list)
-radix_components = defaultdict(list)
 component_list = defaultdict(list)
 docs_ns = SimpleNamespace()
 
@@ -120,11 +119,11 @@ def get_component(doc: str, title: str):
     category = os.path.basename(os.path.dirname(doc)).title()
     d = flexdown.parse_file(doc)
     if doc.startswith("docs/library/chakra"):
+        clist = [title, *get_components_from_metadata(d)]
+        chakra_components[category].append(clist)
         if should_skip_compile(doc):
             outblocks.append((d, route))
             return
-        clist = [title, *get_components_from_metadata(d)]
-        component_list[category].append(clist)
         return multi_docs(path=route, comp=d, component_list=clist, title=title2)
     if doc.startswith("docs/library"):
         clist = [title, *get_components_from_metadata(d)]
@@ -132,7 +131,7 @@ def get_component(doc: str, title: str):
             clist[1],
             (RadixThemesComponent, RadixPrimitiveComponent),
         ):
-            radix_components[category].append(clist)
+            component_list[category].append(clist)
             prefix=""
         elif issubclass(clist[1], ChakraComponent):
             # Workaround for Chakra components outside of chakra directory (like Html).
