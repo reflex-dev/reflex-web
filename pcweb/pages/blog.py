b1d07526ed57d2028ae66842bd6ac395a4a2770b
Nikhil Rao <nikhil@reflex.dev>
2024-02-14 19:17:35 -0800

    Update blog styling

--- pcweb/pages/blog.py
+++ pcweb/pages/blog.py
@@ -72,66 +72,62 @@ def component_grid():
     for path, document in blogs.items():
         meta = document.metadata
         posts.append(
-            rx.link(
-                rx.chakra.box(
-                    height="10rem",
-                    background_image=f'url({meta["image"]})',
-                    background_size="cover",
-                    background_position="center",
-                    background_repeat="no-repeat",
-                    w="100%",
-                ),
-                rx.chakra.box(
-                    rx.chakra.text(
-                        meta["title"],
-                        font_size="1.2rem",
-                        mb="0.5em",
-                        color=tc["docs"]["body"]
-                    ),
-                    rx.chakra.text(
-                        meta["description"],
-                        font_size="0.8rem",
-                        color=tc["docs"]["body"]
-
-                    ),
-                    rx.chakra.divider(),
-                    rx.spacer(),
-                    rx.chakra.hstack(
-                        rx.chakra.vstack(
-                            rx.chakra.text("Written by", font_size="0.6rem"),
-                            rx.chakra.hstack(
-                                rx.chakra.avatar(
-                                    name=meta["author"],
-                                    size="sm",
-                                    bg=c["indigo"][800],
-                                    color="#DACEEE",
-                                ),
-                                rx.chakra.text(meta["author"], font_size="0.8rem"),
-                            ),
-                            align_items="left",
-                        ),
-                        rx.spacer(),
-                        rx.chakra.vstack(
-                            rx.chakra.text("Published on", font_size="0.6rem"),
-                            rx.chakra.text(str(meta["date"]), font_size="0.8em"),
-                            align_items="left",
-                        ),
-                        color=tc["docs"]["body"],
-                        padding_bottom="0.5em",
+            rx.card(
+                rx.el.a(
+                    rx.box(
+                        height="10rem",
+                        background_image=f'url({meta["image"]})',
+                        background_size="cover",
+                        background_position="center",
+                        background_repeat="no-repeat",
                         width="100%",
                     ),
-                    p=4,
-                    height="100%",
-                    width="100%",
+                    rx.vstack(
+                        rx.heading(
+                            meta["title"],
+                            font_size="1.2rem",
+                            margin_bottom="0.5em",
+                        ),
+                        rx.text(
+                            meta["description"],
+                            font_size="0.8rem",
+                        ),
+                        rx.divider(),
+                        rx.hstack(
+                            rx.vstack(
+                                rx.text("Written by", font_size="0.6rem"),
+                                rx.hstack(
+                                    rx.chakra.avatar(
+                                        name=meta["author"],
+                                        size="sm",
+                                        bg=c["indigo"][800],
+                                        color="#DACEEE",
+                                    ),
+                                    rx.text(meta["author"], font_size="0.8rem"),
+                                ),
+                            ),
+                            rx.spacer(),
+                            rx.vstack(
+                                rx.text("Published on", font_size="0.6rem"),
+                                rx.text(str(meta["date"]), font_size="0.8em"),
+                            ),
+                            padding_bottom="0.5em",
+                            width="100%",
+                        ),
+                        padding="1em",
+                        height="100%",
+                        width="100%",
+                    ),
+                    border="1px solid #eee",
+                    border_radius="8px",
+                    overflow="hidden",
+                    bg_color="white",
+                    _hover={
+                        "box_shadow": "0px 0px 0px 1px rgba(52, 46, 92, 0.12), 0px 2px 3px rgba(3, 3, 11, 0.1), 0px 12px 8px rgba(3, 3, 11, 0.04), 0px 8px 12px -4px rgba(3, 3, 11, 0.02)"
+                    },
+                    href=path,
                 ),
-                border="1px solid #eee",
-                border_radius="8px",
-                overflow="hidden",
-                bg_color="white",
-                _hover={
-                    "box_shadow": "0px 0px 0px 1px rgba(52, 46, 92, 0.12), 0px 2px 3px rgba(3, 3, 11, 0.1), 0px 12px 8px rgba(3, 3, 11, 0.04), 0px 8px 12px -4px rgba(3, 3, 11, 0.02)"
-                },
-                href=path,
+                as_child=True,
             ),
         )
     return rx.chakra.box(
@@ -141,33 +137,25 @@ def component_grid():
 
 @webpage(path="/blog", title="Blog")
 def blg():
-    return rx.chakra.container(
-        rx.chakra.vstack(
-            rx.chakra.vstack(
-                rx.chakra.text("Reflex Blog", font_size=styles.H1_FONT_SIZE),
-                rx.chakra.text(
-                    "The latest news from the Reflex team. ",
-                    color=tc["docs"]["body"],
-                ),
-                rx.divider(size="4"),
-                align_items="left",
+    return rx.container(
+        rx.vstack(
+            rx.vstack(
+                rx.heading("Reflex Blog", size="7"),
+                rx.text("The latest news from the Reflex team."),
+                rx.divider(),
                 text_align="left",
                 width="100%",
                 spacing="4",
             ),
             component_grid(),
-            max_width="110em",
-            align_items="stretch",
+            align="stretch",
             min_height="80vh",
             margin_bottom="4em",
-            padding_top="95px",
-            padding_bottom="2em",
-            padding_left = "2em",
-            padding_right = "2em",
+            padding_y="2em",
         ),
+        size="3",
         flex_direction="column",
-        max_width="960px",
-        margin_top="120px"
+        margin_top="120px",
     )
 
 
