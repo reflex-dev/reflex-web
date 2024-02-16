96c7f72e73f0f6a489d235d6b73747f20cc52faf
Alek Petuskey <alekpetuskey@aleks-mbp.lan>
2024-02-16 10:58:56 -0800

    Minor fixes

--- pcweb/components/navbar/buttons/discord.py
+++ pcweb/components/navbar/buttons/discord.py
@@ -4,11 +4,14 @@ from .style import button_style
 
 
 def discord() -> rx.Component:
-    return rx.flex(
-        rx.image(
-            src="/companies/light/discord.svg",
+    return rx.link(
+        rx.flex(
+            rx.image(
+                src="/companies/light/discord.svg",
+            ),
+            padding="7px",
+            style=button_style,
+            border_radius="8px",
         ),
-        padding="7px",
-        style=button_style,
-        border_radius="8px",
+        href="https://discord.gg/T5WSbC2YtQ"
     )
