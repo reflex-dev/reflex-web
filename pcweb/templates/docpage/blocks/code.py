a7ca54d46be30bfd07e8505b7bbcdf9e45c7a0f6
Martin Xu <15661672+martinxu9@users.noreply.github.com>
2024-03-06 09:33:58 -0800

    remove margin around command/output box (#515)

--- pcweb/templates/docpage/blocks/code.py
+++ pcweb/templates/docpage/blocks/code.py
@@ -117,7 +117,6 @@ def doccmdoutput(
         border_radius=styles.DOC_BORDER_RADIUS,
         border=f"2px solid {rx.color('mauve', 3)}",
         position="relative",
-        margin="1em",
         width="100%",
         background_color="black",
     )
