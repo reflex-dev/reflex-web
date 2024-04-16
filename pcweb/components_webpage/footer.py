162243095dbacd197fabb99bbf578d97f9b9ea95
Hongyu Yao <hongyuyao@hongyus-mbp-3.lan>
2024-04-16 15:04:40 -0700

    news letter is up

--- pcweb/components_webpage/footer.py
+++ pcweb/components_webpage/footer.py
@@ -6,6 +6,15 @@ from pcweb.pages.docs.gallery import gallery
 from pcweb.pages.docs.library import library
 from pcweb.pages.index import index
 
+import asyncio
+import json
+import os
+from datetime import datetime
+import httpx
+from email_validator import EmailNotValidError, validate_email
+from sqlmodel import Field
+
+
 footer_item_style = {
     "font_family": styles.SANS,
     "font_weight": "500",
@@ -55,14 +64,14 @@ def installation():
                     rx.text("You should see your first app", color=bottom_color),
                     rx.box(flex_grow=1),
                     rx.button(
-                            "View Docs",
-                            border_radius= "8px;",
-                            background= "linear-gradient(180deg, #6151F3 0%, #5646ED 100%);",
-                            box_shadow= "0px 0px 0px 1px rgba(255, 255, 255, 0.09) inset, 0px 0px 6px 2px rgba(255, 255, 255, 0.12) inset, 0px 2px 24px -1px #080525, 0px 3px 9px -2px #4033C0;",
-                            _hover= {
-                                "border": "1px solid rgba(94, 78, 242, 0.15);"
-                            }
-                        )
+                        "View Docs",
+                        border_radius= "8px;",
+                        background= "linear-gradient(180deg, #6151F3 0%, #5646ED 100%);",
+                        box_shadow= "0px 0px 0px 1px rgba(255, 255, 255, 0.09) inset, 0px 0px 6px 2px rgba(255, 255, 255, 0.12) inset, 0px 2px 24px -1px #080525, 0px 3px 9px -2px #4033C0;",
+                        _hover= {
+                            "border": "1px solid rgba(94, 78, 242, 0.15);"
+                        }
+                    )
                 ),
                 direction="column",
                 spacing="2"
@@ -82,76 +91,176 @@ def installation():
         border_bottom="1px solid #3C3646;"
     )
 
-def footer(style=footer_style):
+class IndexState(rx.State):
+    """Hold the state for the home page."""
+
+    # The waitlist email.
+    email: str
+
+    # Whether the user signed up for the waitlist.
+    signed_up: bool = False
+
+    # Whether to show the confetti.
+    show_confetti: bool = False
+
+    def add_contact_to_loops(self, contact_data):
+        url = "https://app.loops.so/api/v1/contacts/create"
+        loops_api_key = os.getenv("LOOPS_API_KEY")
+        if loops_api_key is None:
+            print("Loops API key does not exist")
+        headers = {
+            "Accept": "application/json",
+            "Authorization": f"Bearer {loops_api_key}",
+        }
+
+        try:
+            with httpx.Client() as client:
+                response = client.post(url, headers=headers, json=contact_data)
+                response.raise_for_status()  # Raise an exception for HTTP errors (4xx and 5xx)
+
+        except httpx.RequestError as e:
+            print(f"An error occurred: {e}")
+    
+    def signup(self):
+        """Sign the user up for the waitlist."""
+        # Check if the email is valid.
+        try:
+            validation = validate_email(self.email, check_deliverability=True)
+            self.email = validation.email
+        except EmailNotValidError as e:
+            # Alert the error message.
+            return rx.window_alert(str(e))
+
+        # Check if the user is already on the waitlist.
+        with rx.session() as session:
+            user = session.query(Waitlist).filter(Waitlist.email == self.email).first()
+            if user is None:
+                # Add the user to the waitlist.
+                session.add(Waitlist(email=self.email))
+                session.commit()
+                contact_data = json.dumps({"email": self.email})
+                self.add_contact_to_loops(contact_data)
+
+        self.signed_up = True
+        return IndexState.play_confetti
+
+    async def play_confetti(self):
+        """Play confetti for 5sec then stop."""
+        self.show_confetti = True
+        yield
+        await asyncio.sleep(5)
+        self.show_confetti = False
+        yield
+
+def news_letter():
+    return rx.vstack(
+        rx.text("Subscribe to newsletter", color="#6C6C81"),
+        rx.chakra.input_group(
+            rx.chakra.input_right_element(
+                rx.chakra.button(
+                    "->",
+                    color="#FFF",
+                    on_click=IndexState.signup,
+                    background="linear-gradient(180deg, #6151F3 0%, #5646ED 100%);",
+                    border_top_left_radius="0px",
+                    border_bottom_left_radius="0px",
+                    _hover={"linear-gradient(180deg, #6151F3 0%, #5646ED 100%);"},
+                )           
+            ),
+            rx.chakra.input(
+                placeholder="Email...",
+                on_blur=IndexState.set_email,
+                color="#6C6C81",
+                type="email",
+                border="1px solid #3455eb",
+                _focus={"border": "1px solid #82799E"},
+                border_radius="8px",
+            ),
+        ),
+        align_items="left",
+    )
+
+
+
+
+def links():
     from pcweb.pages.blog import blg
     from pcweb.pages.changelog import changelog
     from pcweb.pages.faq import faq
+    return rx.hstack(
+        rx.desktop_only(
+            logo(
+                width=["5em", "6em", "7em"],
+            ),
+        ),
+        rx.vstack(
+            rx.text("Site", color="#E8E8F4"),
+            rx.link("Home", href=index.path, style=footer_item_style),
+            rx.link("Gallery", href=gallery.path, style=footer_item_style),
+            rx.link("Blog", href=blg.path, style=footer_item_style),
+            rx.link(
+                "Changelog",
+                href=changelog.path,
+                style=footer_item_style,
+            ),
+            align_items="start",
+        ),
+        rx.vstack(
+            rx.text("Documentation", color="#E8E8F4"),
+            rx.link(
+                "Introduction",
+                href=getting_started.introduction.path,
+                style=footer_item_style,
+            ),
+            rx.link(
+                "Installation",
+                href=getting_started.installation.path,
+                style=footer_item_style,
+            ),
+            rx.link(
+                "Components",
+                href=library.path,
+                style=footer_item_style,
+            ),
+            align_items="start",
+        ),
+        rx.vstack(
+            rx.text("Resources", color="#E8E8F4"),
+            rx.link(
+                "FAQ",
+                href=faq.path,
+                style=footer_item_style,
+            ),
+            rx.link(
+                "Roadmap",
+                href=constants.ROADMAP_URL,
+                style=footer_item_style,
+            ),
+            rx.link(
+                "Forum",
+                href=constants.GITHUB_DISCUSSIONS_URL,
+                style=footer_item_style,
+            ),
+            align_items="start",
+        ),
+        rx.desktop_only(
+            news_letter(),
+        ),
+        justify="between",
+        align_items="top",
+        padding_bottom="2em",
+        min_width="100%",
+        padding_top="2em"
+    )
 
+
+
+def footer(style=footer_style):
     return rx.box(
         rx.vstack(
             installation(),
-            rx.hstack(
-                rx.desktop_only(
-                    logo(
-                        width=["5em", "6em", "7em"],
-                    ),
-                ),
-                rx.vstack(
-                    rx.text("Site", color="#E8E8F4"),
-                    rx.link("Home", href=index.path, style=footer_item_style),
-                    rx.link("Gallery", href=gallery.path, style=footer_item_style),
-                    rx.link("Blog", href=blg.path, style=footer_item_style),
-                    rx.link(
-                        "Changelog",
-                        href=changelog.path,
-                        style=footer_item_style,
-                    ),
-                    align_items="start",
-                ),
-                rx.vstack(
-                    rx.text("Documentation", color="#E8E8F4"),
-                    rx.link(
-                        "Introduction",
-                        href=getting_started.introduction.path,
-                        style=footer_item_style,
-                    ),
-                    rx.link(
-                        "Installation",
-                        href=getting_started.installation.path,
-                        style=footer_item_style,
-                    ),
-                    rx.link(
-                        "Components",
-                        href=library.path,
-                        style=footer_item_style,
-                    ),
-                    align_items="start",
-                ),
-                rx.vstack(
-                    rx.text("Resources", color="#E8E8F4"),
-                    rx.link(
-                        "FAQ",
-                        href=faq.path,
-                        style=footer_item_style,
-                    ),
-                    rx.link(
-                        "Roadmap",
-                        href=constants.ROADMAP_URL,
-                        style=footer_item_style,
-                    ),
-                    rx.link(
-                        "Forum",
-                        href=constants.GITHUB_DISCUSSIONS_URL,
-                        style=footer_item_style,
-                    ),
-                    align_items="start",
-                ),
-                justify="between",
-                align_items="top",
-                padding_bottom="3em",
-                min_width="100%",
-                padding_top="4em"
-            ),
+            links(),
+            rx.mobile_only(news_letter()),
             rx.hstack(
                 rx.text(
                     "Copyright © 2024 Pynecone, Inc.",
@@ -180,6 +289,7 @@ def footer(style=footer_style):
                     ),
                     gap="1em",
                 ),
+                padding_top="2em",
                 justify="between",
                 color=rx.color("mauve", 5),
                 padding_bottom="2em",
