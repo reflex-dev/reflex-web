63822df324d3cd510f179cb749e4962ad81ce26c
Martin Xu <15661672+martinxu9@users.noreply.github.com>
2024-04-16 14:57:26 -0700

    In mobile view, main site top right menu button opens main nav bar instead of docs (#621)
    * refactoring
    
    * refactoring
    
    * landing page almost there
    
    * change menu button click to show same navbar instead of docs
    
    * unused code
    
    * unwanted merge
    
    * oops
    
    * update roadmap URL (use github issue link) and use constants for references
    
    * drive-by: fix contributing URL as well
    
    * change to pure white
    
    ---------
    
    Co-authored-by: Hongyu Yao <hongyuyao@Hongyus-MacBook-Pro-3.local>
    Co-authored-by: Hongyu Yao <hongyuyao@hongyus-mbp-3.lan>

--- pcweb/components_webpage/sidebar/sidebar.py
+++ pcweb/components_webpage/sidebar/sidebar.py
@@ -4,13 +4,45 @@ from __future__ import annotations
 
 import reflex as rx
 
+from pcweb import constants
+from pcweb.pages.docs import getting_started, hosting
+from pcweb.pages.faq import faq
+from pcweb.pages.docs.library import library
+from pcweb.pages.blog import blg
+from pcweb.pages.changelog import changelog
+from pcweb.pages.docs.gallery import gallery
+
 
 def sidebar(url=None, width: str = "100%") -> rx.Component:
     """Render the sidebar."""
 
+    section_style = {
+        "color": "#ffffff",
+        "font-weight": "400",
+    }
 
-    return rx.flex(
-        
+    return rx.vstack(
+        rx.link("Blog", href=blg.path, style=section_style),
+        rx.link("Gallery", href=gallery.path, style=section_style),
+        rx.link("Docs", href=getting_started.introduction.path, style=section_style),
+        rx.link("Component Library", href=library.path, style=section_style),
+        rx.link("Changelog", href=changelog.path, style=section_style),
+        rx.link(
+            "Roadmap",
+            href=constants.ROADMAP_URL,
+            style=section_style,
+        ),
+        rx.link(
+            "Deployment", href=hosting.deploy_quick_start.path, style=section_style
+        ),
+        rx.link("Self-Hosting", href=hosting.self_hosting.path, style=section_style),
+        rx.link(
+            "Contribute",
+            href=constants.CONTRIBUTING_URL,
+            style=section_style,
+        ),
+        rx.link("FAQ", href=faq.path, style=section_style),
+        spacing="5",
         width="100%",
         height="100%",
         justify="end",
