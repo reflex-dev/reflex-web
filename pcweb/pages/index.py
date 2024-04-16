58ad7978fb1db7a86b923de0ca39096a06d50b2e
HongyuHansonYao <159659100+HongyuHansonYao@users.noreply.github.com>
2024-04-16 14:36:33 -0700

    fixed safari view (#624)
    Co-authored-by: Hongyu Yao <hongyuyao@hongyus-mbp-3.lan>

--- pcweb/pages/index.py
+++ pcweb/pages/index.py
@@ -331,7 +331,7 @@ def request_buttons() -> rx.Component:
             "Bugs",
             color="#2BCEEA",
             weight="Medium",
-            height="22px",
+            height="24px",
             width="138px",
             border="1px solid #2BCEEA",
             background_color="rgba(43, 206, 234, 0.25)",
@@ -348,7 +348,6 @@ def request_buttons() -> rx.Component:
             color="#2BEA8E",
             weight="Medium",
             height="24px",
-            width="138px",
             border="1px solid #2BEA8E",
             background_color="rgba(43, 234, 142, 0.25)",
             on_click=rx.redirect(
