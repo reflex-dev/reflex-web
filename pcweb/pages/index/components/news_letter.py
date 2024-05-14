06dcc989d0657d767b754b18a1b16ee5e35a8ae8
Hongyu Yao <hongyuyao@Hongyus-MacBook-Pro-3.local>
2024-05-13 17:42:59 -0700

    fixed newletter width, fixed whitelist

--- pcweb/pages/index/components/news_letter.py
+++ pcweb/pages/index/components/news_letter.py
@@ -26,38 +26,41 @@ def news_letter_text() -> rx.Component:
 
 
 def news_letter_section() -> rx.Component:
-    return rx.vstack(
-        news_letter_text(),
-        rx.chakra.input_group(
-            rx.chakra.input_right_element(
-                rx.chakra.button(
-                    "Subscribe ->",
-                    color="#FFF",
-                    # on_click=IndexState.signup,
-                    background="linear-gradient(180deg, #6151F3 0%, #5646ED 100%)",
-                    box_shadow="0px 2px 9px -4px rgba(64, 51, 192, 0.70), 0px 0px 6px 2px rgba(255, 255, 255, 0.12) inset, 0px 0px 0px 1px rgba(255, 255, 255, 0.09) inset",
-                    border_left="2px solid rgba(186, 199, 247, 0.12)",
-                    border_top_left_radius="0px",
-                    border_bottom_left_radius="0px",
-                    size="lg",
-                    _hover={"linear-gradient(180deg, #6151F3 0%, #5646ED 100%);"},
+    return rx.center(
+        rx.vstack(
+            news_letter_text(),
+            rx.chakra.input_group(
+                rx.chakra.input_right_element(
+                    rx.chakra.button(
+                        "Subscribe ->",
+                        color="#FFF",
+                        # on_click=IndexState.signup,
+                        background="linear-gradient(180deg, #6151F3 0%, #5646ED 100%)",
+                        box_shadow="0px 2px 9px -4px rgba(64, 51, 192, 0.70), 0px 0px 6px 2px rgba(255, 255, 255, 0.12) inset, 0px 0px 0px 1px rgba(255, 255, 255, 0.09) inset",
+                        border_left="2px solid rgba(186, 199, 247, 0.12)",
+                        border_top_left_radius="0px",
+                        border_bottom_left_radius="0px",
+                        size="lg",
+                        _hover={"linear-gradient(180deg, #6151F3 0%, #5646ED 100%);"},
+                    ),
+                    height="100%",
+                    width="25%",
+                    align="left",
                 ),
-                height="100%",
-                width="20%",
-                align="left",
-            ),
-            rx.chakra.input(
-                placeholder="Enter your email address here",
-                # on_blur=IndexState.set_email,
-                color="#fff",
-                background="rgba(161, 157, 213, 0.03)",
-                border="2px solid rgba(186, 199, 247, 0.12)",
-                type="email",
-                border_radius="8px",
+                rx.chakra.input(
+                    placeholder="Enter your email address here",
+                    # on_blur=IndexState.set_email,
+                    color="#fff",
+                    background="rgba(161, 157, 213, 0.03)",
+                    border="2px solid rgba(186, 199, 247, 0.12)",
+                    type="email",
+                    border_radius="8px",
+                    height="48px",
+                ),
+                width="100%",
                 height="48px",
             ),
-            width="100%",
-            height="48px",
-        ),
-        padding = "3em",
+            width="80%",
+            padding = "3em",
+        )
     )
