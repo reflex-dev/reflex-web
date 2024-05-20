fe06265f991edf3e70c749c7a2e315661cd76541
Hongyu Yao <hongyuyao@hongyus-mbp-3.lan>
2024-05-20 15:58:45 -0700

    updated Recipe landing template

--- pcweb/pages/docs/recipes_overview.py
+++ pcweb/pages/docs/recipes_overview.py
@@ -4,29 +4,45 @@ from pcweb.templates.docpage import docpage, h1_comp, text_comp
 
 
 
-def component_grid():
-    from pcweb.components.docpage.sidebar import get_component_link
-    from pcweb.pages.docs import component_list
+items = [
+    "navbar",
+    "footer",
+    "header",
+    "hero section",
+    "CTA buttons",
+    "forms",
+    "image gallery",
+    "testimonials",
+    "blog posts",
+    "social media icons",
+    "search bar",
+    "breadcrumbs",
+    "pagination",
+    "modal windows",
+    "accordion",
+    "tabs",
+    "progress bars",
+    "timeline",
+    "pricing tables",
+    "FAQ section"
+]
 
+def component_grid():
     sidebar = []
-    for category in component_list:
+    for item in items:
         sidebar.append(
             rx.box(
-                rx.heading(category, style={"fontSize": "1.5em"}),
-                rx.divider(),
-                rx.vstack(
-                    *[
-                        rx.link(
-                            rx.utils.format.to_title_case(c[0]),
-                            href=get_component_link(category, c),
-                            font_size="1em",
-                            color=rx.color("mauve", 12),
-                            _hover={"color": rx.color("violet", 9)},
-                        )
-                        for c in component_list[category]
-                    ],
-                    align_items="start",
+                rx.icon("layout-template"),
+                rx.heading(
+                    rx.link(
+                        item, 
+                        href="https://www.miffy.com/",
+                        color=rx.color("mauve", 11),
+                    ),
+                    margin_top="0.75em",
+                    style={"fontSize": "1.25em"},
                 ),
+                rx.text("5 variants", color=rx.color("mauve", 11)),
                 row_span=3,
                 col_span=1,
                 border_radius="8px",
@@ -46,7 +62,7 @@ def overview():
     return rx.flex(
         h1_comp(text="Recipes"),
         text_comp(
-            text="Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. This page contains a list of all builtin components. "
+            text="Reflex comes with a wide range of pre-built components that are designed to help you quickly create modern, responsive web pages."
         ),
         component_grid(),
         text_align="left",
