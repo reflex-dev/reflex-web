62b515bff461004ed8296baba307ec8230489592
Hongyu Yao <hongyuyao@Hongyus-MacBook-Pro-3.local>
2024-05-23 09:05:45 -0700

    updated struture of overview page

--- pcweb/pages/docs/recipes_overview.py
+++ pcweb/pages/docs/recipes_overview.py
@@ -3,8 +3,21 @@ import reflex as rx
 
 from pcweb.templates.docpage import docpage, h1_comp, text_comp
 
+def get_component_link(category, clist) -> str:
+    file_name_without_extension = clist.split('/')[-1].split('.')[0]
+    return f"/docs/recipes/{category}/{file_name_without_extension}"
+
+def format_titles(path):
+    title_without_ext = path.split('.')[0]
+    parts = title_without_ext.split('/')
+    last_part = parts[-1]
+    capitalized_last_part = last_part.replace('_', '-').title()
+    return capitalized_last_part
+
+
 def component_grid():
     from pcweb.pages.docs import recipes_list
+
     sidebar = []
     for item in recipes_list:
         category = item.split('/')[-1]
@@ -23,7 +36,19 @@ def component_grid():
                     margin_top="0.75em",
                     style={"fontSize": "1.25em"},
                 ),
-                rx.text("5 variants", color=rx.color("mauve", 11)),
+                rx.vstack(
+                    *[
+                        rx.link(
+                            format_titles(c),
+                            href=get_component_link(category, c),
+                            font_size="1em",
+                            color=rx.color("mauve", 12),
+                            _hover={"color": rx.color("violet", 9)},
+                        )
+                        for c in recipes_list[category]
+                    ],
+                    align_items="start",
+                ),
                 row_span=3,
                 col_span=1,
                 border_radius="8px",
