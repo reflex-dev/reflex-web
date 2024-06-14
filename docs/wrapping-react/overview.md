ca359365d13b4d6c01762d9a53f4e27c1a68bc9c
Masen Furer <m_github@0x26.net>
2024-06-14 12:34:46 -0700

    Pin splinetool/runtime@1.5.5 (#760)
    Versions beyond this raise a client side error, unclear why

--- docs/wrapping-react/overview.md
+++ docs/wrapping-react/overview.md
@@ -28,10 +28,10 @@ class Spline(rx.Component):
     """Spline component."""
 
     # The name of the npm package.
-    library = "@splinetool/react-spline@^2.2.6"
+    library = "@splinetool/react-spline"
 
     # Any additional libraries needed to use the component.
-    lib_dependencies: list[str] = ["@splinetool/runtime"]
+    lib_dependencies: list[str] = ["@splinetool/runtime@1.5.5"]
 
     # The name of the component to use from the package.
     tag = "Spline"
--- pcweb/components/spline.py
+++ pcweb/components/spline.py
@@ -5,12 +5,12 @@ from reflex.vars import Var
 class Spline(rx.Component):
     """Spline component."""
 
-    library = "@splinetool/react-spline@^2.2.6"
+    library = "@splinetool/react-spline"
     tag = "Spline"
     scene: Var[str] = "https://prod.spline.design/Br2ec3WwuRGxEuij/scene.splinecode"
     is_default = True
 
-    lib_dependencies: list[str] = ["@splinetool/runtime"]
+    lib_dependencies: list[str] = ["@splinetool/runtime@1.5.5"]
 
 
 spline = Spline.create
