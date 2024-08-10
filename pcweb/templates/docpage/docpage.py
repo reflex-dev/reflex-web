cc6c9c305b17b4a6bd65b575e696603fe270ec60
parthamv <mvgopesh@gmail.com>
2024-08-10 19:14:41 -0400

    Initial Check

--- pcweb/templates/docpage/docpage.py
+++ pcweb/templates/docpage/docpage.py
@@ -98,9 +98,8 @@ def docpage_footer(path: str):
     from pcweb.pages.docs import getting_started, hosting
     from pcweb.pages.changelog import changelog
 
-    path = str(path)
-    if len(path) > 0 and path[-1] == '/':
-        path = path[:-1]
+    
+    path = path[:-1]
     return rx.flex(
         rx.divider(size="4"),
         rx.flex(
