9643927d854da7939098c742375d6f0c5eec8aad
elvis kahoro <elvis@reflex.dev>
2024-08-28 19:14:46 -0700

    feat: Adding Common Room pixel for website tracking

--- pcweb/scripts.py
+++ pcweb/scripts.py
@@ -1,6 +1,27 @@
 import reflex as rx
 
-
+PIXEL_COMMON_ROOM_SCRIPT: str = """
+<script>
+  (function() {
+    if (typeof window === 'undefined') return;
+    if (typeof window.signals !== 'undefined') return;
+    var script = document.createElement('script');
+    script.src = 'https://cdn.cr-relay.com/v1/site/b3e8ad62-9c28-4ad4-b014-925c55d56334/signals.js';
+    script.async = true;
+    window.signals = Object.assign(
+      [],
+      ['page', 'identify', 'form'].reduce(function (acc, method){
+        acc[method] = function () {
+          signals.push([method, arguments]);
+          return signals;
+        };
+       return acc;
+      }, {})
+    );
+    document.head.appendChild(script);
+  })();
+</script>
+"""
 PIXEL_GOOGLE_TAG_MANAGER_RUNNER_SCRIPT: str = """
 window.dataLayer = window.dataLayer || [];
 function gtag() {
@@ -56,5 +77,8 @@ def get_pixel_website_trackers() -> list[rx.Component]:
         ),
         rx.script(
             PIXEL_POSTHOG_SCRIPT,
+        ),
+        rx.el.script(
+            PIXEL_COMMON_ROOM_SCRIPT,
         ),
     ]
