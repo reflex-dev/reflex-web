845c6e7aa8a149053344a345a77fbfa2860affad
Carlos <cutillascarlos@gmail.com>
2024-09-13 18:20:32 +0200

    heading .

--- pcweb/pages/index/views/hosting_features.py
+++ pcweb/pages/index/views/hosting_features.py
@@ -56,7 +56,7 @@ def hosting_grid() -> rx.Component:
         rx.box(
             rx.box(
                 rx.el.h3(
-                    "Deploy your app with a single command",
+                    "Deploy your app with a single command.",
                     class_name="font-x-large text-slate-12 text-right",
                 ),
                 rx.el.p(
