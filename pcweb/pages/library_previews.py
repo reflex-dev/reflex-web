72d944d0621a9b167cab6bf3ea8ccece64073fba
Alek Petuskey <alekpetuskey@Aleks-MacBook-Pro.local>
2024-09-15 17:40:14 -0700

    Fix page title

--- pcweb/pages/library_previews.py
+++ pcweb/pages/library_previews.py
@@ -46,7 +46,7 @@ def create_previews(
     prefix: str = "",
     type: str = "core",
 ):
-    @docpage(right_sidebar=False, set_path=f"/docs/library{prefix.strip('/')}/" + path.strip('/'), page_title=component_category)
+    @docpage(right_sidebar=False, set_path=f"/docs/library{prefix.strip('/')}/" + path.strip('/'), page_title=component_category + " Library")
     def page() -> rx.Component:
         from pcweb.components.docpage.sidebar.sidebar_items import get_component_link
 
