889d21e317c94bc962c43eb91f2b18742e15d8b3
Alek Petuskey <alekpetuskey@Aleks-MacBook-Pro.local>
2024-09-19 19:04:27 -0700

    Update stats

--- pcweb/pages/customers/views/hero.py
+++ pcweb/pages/customers/views/hero.py
@@ -7,11 +7,11 @@ def hero() -> rx.Component:
         rx.text.span("Customers", class_name="text-slate-9 font-smbold text-center font-small-smbold -mb-2"),
         rx.el.h1(
             """Meet the teams
-who build the future""",
+who chose Reflex""",
             class_name="max-w-full inline-block bg-clip-text bg-gradient-to-r from-slate-12 to-slate-11 w-full font-xx-large text-center text-transparent text-balance whitespace-pre mx-auto break-words",
         ),
         rx.el.h2(
-            """Tens of thousands of companies use Reflex to build better. 
+            """Thousands of companies use Reflex to build better. 
 From next-gen startups to established enterprises.""",
             class_name="max-w-full w-full font-md-smbold text-center text-slate-9 whitespace-pre mx-auto text-balance word-wrap break-words",
         ),
--- pcweb/pages/customers/views/stats.py
+++ pcweb/pages/customers/views/stats.py
@@ -13,16 +13,13 @@ def stats_grid() -> rx.Component:
     return rx.box(
         rx.box(
             rx.text(
-                "Teams that switch to Reflex create more issues and close them faster",
+                "With Reflex you can start with an mvp and scale up to a full fledged production application",
                 class_name="font-base text-slate-9 max-w-[12.875rem] text-center lg:text-left",
             ),
             class_name="p-10 lg:border-l border-slate-3 flex justify-center lg:justify-start",
         ),
-        stat_card(stat="98.5%", text="Increase in filed issues"),
-        stat_card(
-            stat="24x",
-            text="Faster to close issues",
-        ),
+        stat_card(stat="100+", text="Teams using Reflex in a production environment"),
+        stat_card(stat="5000+", text="New applications built with Reflex every month"),
         class_name="grid grid-cols-1 lg:grid-cols-3 gap-0 grid-rows-1 w-full divide-slate-3 lg:divide-x !border-t-0 lg:border-b border-slate-3 lg:border-r divide-y lg:divide-y-0 [&>:last-child]:!border-b lg:[&>:last-child]:!border-b-0",
     )
 
