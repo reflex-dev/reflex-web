d12175cbc68aa915914b95055002a43b28c6b67a
Carlos <36110765+carlosabadia@users.noreply.github.com>
2024-09-20 04:06:45 +0200

    [DEVX-470] Create case studies customers page (#925)
    * init
    
    * No filters applied text
    
    * reorganize folders and mobile view clean up
    
    * fix f-string
    
    * comment out companies
    
    * Update stats
    
    ---------
    
    Co-authored-by: Alek Petuskey <alekpetuskey@Aleks-MacBook-Pro.local>

---
+++ pcweb/pages/customers/data/customers.py
@@ -0,0 +1,48 @@
+import reflex as rx
+import flexdown
+from pcweb.flexdown import xd2 as xd
+from pcweb.templates.storypage import storypage
+
+
+
+def content(document):
+    return rx.box(
+        xd.render(document, "blog.md"),
+    ),
+
+CUSTOMERS_PATH = "case-studies/"
+
+def get_customer_data(paths):
+    customers = {}
+    for path in reversed(sorted(paths)):
+        document = flexdown.parse_file(path)
+        path = path.replace(".md", "/")
+        customers[path] = document
+    return customers
+
+
+def get_route(path: str):
+    """Get the route for a page."""
+    return path.replace(CUSTOMERS_PATH, "").replace(".md", "")
+
+
+paths = flexdown.utils.get_flexdown_files(CUSTOMERS_PATH)
+customer_data = get_customer_data(paths)
+
+customers_routes = []
+for path, document in customer_data.items():
+    # Get the docpage component.
+    route = f"/customers/{document.metadata['company'].lower()}"
+    title = rx.utils.format.to_snake_case(path.rsplit("/", 1)[1].replace(".md", ""))
+    comp = storypage(
+        path=route,
+        company=document.metadata["company"],
+        description=document.metadata["description"],
+        domain=document.metadata["domain"],
+        founded=document.metadata["founded"],
+        investors=document.metadata["investors"],
+        meta=document.metadata["meta"],
+    )(lambda doc=document: content(document))
+
+    # # Add the route to the list of routes.
+    customers_routes.append(comp)
---
+++ pcweb/pages/customers/views/hero.py
@@ -0,0 +1,19 @@
+import reflex as rx
+
+
+def hero() -> rx.Component:
+    return rx.el.section(
+        # Headings
+        rx.text.span("Customers", class_name="text-slate-9 font-smbold text-center font-small-smbold -mb-2"),
+        rx.el.h1(
+            """Meet the teams
+who chose Reflex""",
+            class_name="max-w-full inline-block bg-clip-text bg-gradient-to-r from-slate-12 to-slate-11 w-full font-xx-large text-center text-transparent text-balance whitespace-pre mx-auto break-words",
+        ),
+        rx.el.h2(
+            """Thousands of companies use Reflex to build better. 
+From next-gen startups to established enterprises.""",
+            class_name="max-w-full w-full font-md-smbold text-center text-slate-9 whitespace-pre mx-auto text-balance word-wrap break-words",
+        ),
+        class_name="flex flex-col justify-center items-center gap-4 mx-auto w-full max-w-[64.19rem] lg:border-x border-slate-3 pb-[2.5rem] pt-24 lg:pt-48",
+    )
