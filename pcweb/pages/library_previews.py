7cb7aea9e41df7b39a9804be5c7e38c395de21b6
Tom Gotsman <tomgotsman@Toms-MacBook-Pro.local>
2024-09-25 16:22:28 -0700

    update table to table and grids

--- pcweb/pages/library_previews.py
+++ pcweb/pages/library_previews.py
@@ -126,10 +126,10 @@ core_components_dict = {
         "description": "Components that help with overlays, such as modals, popovers, and tooltips. These are useful for creating responsive and interactive user interfaces.",
         "component_category": "Overlay",
     },
-    "table": {
-        "path": "table",
+    "tables_and_grids": {
+        "path": "tables-and-grids",
         "description": "Powerful table components for organizing and displaying data efficiently. Includes versatile options like standard tables, interactive datatables, and editable data grids. Perfect for creating responsive, user-friendly interfaces that present information clearly and allow for easy data manipulation.",
-        "component_category": "Table",
+        "component_category": "Tables-And-Grids",
     },
     "typography": {
         "path": "typography",
