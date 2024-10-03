23119e43ca78498b2ab43fc60a1992d4f57a68c5
carlosabadia <cutillascarlos@gmail.com>
2024-10-03 22:43:33 +0200

    change ogimage path

--- pcweb/meta/meta.py
+++ pcweb/meta/meta.py
@@ -4,21 +4,33 @@ import reflex as rx
 meta_tags = [
     # HTML Meta Tags
     {"name": "application-name", "content": "Reflex"},
-    {"name": "keywords", "content": "reflex, python, web apps, framework, open source, frontend, backend, full stack"},
-    {"name": "description", "content": "The open-source framework to build and deploy web apps using Python."},
+    {
+        "name": "keywords",
+        "content": "reflex, python, web apps, framework, open source, frontend, backend, full stack",
+    },
+    {
+        "name": "description",
+        "content": "The open-source framework to build and deploy web apps using Python.",
+    },
     # Facebook Meta Tags
     {"property": "og:url", "content": "https://reflex.dev/"},
     {"property": "og:type", "content": "website"},
     {"property": "og:title", "content": "Reflex · Web apps in Pure Python"},
-    {"property": "og:description", "content": "The open-source framework to build and deploy web apps using Python."},
-    {"property": "og:image", "content": "https://reflex.dev/ogimage.jpg"},
+    {
+        "property": "og:description",
+        "content": "The open-source framework to build and deploy web apps using Python.",
+    },
+    {"property": "og:image", "content": "/previews/index_preview.png"},
     # Twitter Meta Tags
     {"name": "twitter:card", "content": "summary_large_image"},
     {"property": "twitter:domain", "content": "reflex.dev"},
     {"property": "twitter:url", "content": "https://reflex.dev/"},
     {"name": "twitter:title", "content": "Reflex · Web apps in Pure Python"},
-    {"name": "twitter:description", "content": "The open-source framework to build and deploy web apps using Python."},
-    {"name": "twitter:image", "content": "https://reflex.dev/ogimage.jpg"},
+    {
+        "name": "twitter:description",
+        "content": "The open-source framework to build and deploy web apps using Python.",
+    },
+    {"name": "twitter:image", "content": "/previews/index_preview.png"},
     {"name": "twitter:creator", "content": "@getreflex"},
 ]
 
