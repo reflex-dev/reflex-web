8c40b70f7a7d77d5ed6e103fae65110a7acf1a6e
Carlos <cutillascarlos@gmail.com>
2024-10-14 12:01:03 +0200

    [GTM-603] Expand codeblock

--- pcweb/templates/docpage/blocks/demo.py
+++ pcweb/templates/docpage/blocks/demo.py
@@ -3,8 +3,44 @@
 import reflex as rx
 import textwrap
 from typing import Any, Callable
-from .code import code_block, code_block_dark
+from .code import code_block, code_block_dark, code_block_expanded
 import black
+from pcweb.components.button import button
+
+
+def expand_code_script(id: str):
+    """Copy script for the code block.
+    Args:
+        id (str): The ID of the button element.
+    Returns:
+        Any: The result of calling the script.
+    """
+    return rx.call_script(
+        f"""
+const button = document.getElementById('{id}');
+const collapsedCode = button.nextElementSibling;
+const expandedCode = collapsedCode.nextElementSibling;
+button.addEventListener('click', () => {{
+    if (collapsedCode.classList.contains('hidden')) {{
+        collapsedCode.classList.remove('hidden');
+        expandedCode.classList.add('hidden');
+        button.textContent = 'Expand Code';
+    }} else {{
+        collapsedCode.classList.add('hidden');
+        expandedCode.classList.remove('hidden');
+        button.textContent = 'Collapse Code';
+    }}
+}});
+"""
+    )
+
+
+def expand_button(id: str) -> rx.Component:
+    return button(
+        "Expand Code",
+        class_name="!w-fit scale-90 self-end",
+        id=id,
+    )
 
 
 def docdemobox(*children, **props) -> rx.Component:
@@ -25,6 +61,7 @@ def docdemobox(*children, **props) -> rx.Component:
 
 def doccode(
     code: str,
+    code_copy: str = "",
     language: str = "python",
     lines: tuple[int, int] | None = None,
     theme: str = "light",
@@ -33,6 +70,7 @@ def doccode(
 
     Args:
         code: The code to display.
+        code_copy: The original code to copy.
         language: The language of the code.
         lines: The start/end lines to display.
         props: Props to apply to the code snippet.
@@ -54,9 +92,11 @@ def doccode(
 
     # Create the code snippet.
     cb = code_block_dark if theme == "dark" else code_block
+    cb = code_block_expanded if code_copy != "" else cb
     return cb(
         code=code,
         language=language,
+        code_copy=code_copy,
     )
 
 
@@ -67,6 +107,7 @@ def docdemo(
     context: bool = False,
     demobox_props: dict[str, Any] | None = None,
     theme: str | None = None,
+    expanded: bool = False,
     **props,
 ) -> rx.Component:
     """Create a documentation demo with code and output.
@@ -82,6 +123,20 @@ def docdemo(
         The styled demo.
     """
     demobox_props = demobox_props or {}
+    if expanded:
+        code_copy = code
+        code = "\n".join(line for line in code.split("\n") if "\\start" not in line and "\\end" not in line)
+
+        lines = code_copy.split("\n")
+        start_line = next((i for i, line in enumerate(lines) if "\\start" in line), None)
+        end_line = next((i for i, line in enumerate(lines) if "\\end" in line), None)
+
+        if start_line is not None and end_line is not None:
+            collapsed_code = "\n".join(lines[start_line + 1 : end_line])
+        else:
+            collapsed_code = code
+        button_id = f"expand-button-{hash(collapsed_code)}"
+
     # Render the component if necessary.
     if comp is None:
         comp = eval(code)
@@ -119,17 +174,52 @@ def docdemo(
                 rx.box(docdemobox(comp, **(demobox_props or {})), class_name="my-4"),
                 value="tab1",
             ),
-            rx.tabs.content(
-                rx.box(doccode(code, theme=theme), class_name="my-4"),
-                value="tab2",
+            # Add the expand button and the collapsed code
+            (
+                rx.tabs.content(
+                    rx.box(
+                        expand_button(button_id),
+                        # Collapsed code
+                        rx.box(
+                            doccode(collapsed_code, theme=theme, code_copy=code),
+                            class_name="",
+                        ),
+                        # Expanded code
+                        rx.box(
+                            doccode(code, theme=theme, code_copy=code),
+                            class_name="hidden",
+                        ),
+                        on_mount=expand_code_script(button_id),
+                        spacing="4",
+                        width="100%",
+                        class_name="mt-4 relative flex flex-col",
+                    ),
+                    value="tab2",
+                )
+                if expanded
+                else rx.tabs.content(
+                    rx.box(
+                        doccode(code, theme=theme),
+                    ),
+                    value="tab2",
+                )
             ),
             default_value="tab1",
         )
+
     # Create the demo.
     return rx.box(
         docdemobox(comp, **(demobox_props or {})),
-        doccode(code, theme=theme),
-        class_name="py-4 gap-4 flex flex-col w-full",
+        rx.box(
+            expand_button(button_id) if demobox_props.pop("expand", False) else None,
+            rx.box(
+                doccode(code, theme=theme, code_copy=code),
+                class_name="hidden" if demobox_props.get("expand", False) else "",
+            ),
+            spacing="4",
+            width="100%",
+        ),
+        class_name="py-4 gap-4 flex flex-col w-full relative",
         **props,
     )
 
@@ -151,9 +241,7 @@ def docgraphing(
                 justify_content="end",
             ),
             rx.box(
-                rx.tabs.content(
-                    doccode(code), value="code", class_name="w-full px-0"
-                ),
+                rx.tabs.content(doccode(code), value="code", class_name="w-full px-0"),
                 rx.tabs.content(
                     doccode(data or ""), value="data", class_name="w-full px-0"
                 ),
