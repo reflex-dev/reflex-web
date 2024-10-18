4632fd40971d6631d9c9fd081e27aac9cbb3eac9
elvis kahoro <elvis@reflex.dev>
2024-10-17 17:15:32 -0700

    feat: Adding koala pixel

--- pcweb/telemetry/pixels.py
+++ pcweb/telemetry/pixels.py
@@ -3,13 +3,7 @@ from __future__ import annotations
 import itertools
 from typing import TYPE_CHECKING
 
-from pcweb.telemetry import (
-    pixels_clearbit,
-    pixels_common_room,
-    pixels_google,
-    pixels_instantly,
-    pixels_posthog,
-)
+from pcweb.telemetry import pixels_clearbit, pixels_google, pixels_koala, pixels_posthog
 
 if TYPE_CHECKING:
     import reflex as rx
@@ -20,9 +14,10 @@ def get_pixel_website_trackers() -> list[rx.Component]:
     return list(
         itertools.chain(
             pixels_google.get_pixel_website_trackers(),
-            pixels_common_room.get_pixel_website_trackers(),
+            # pixels_common_room.get_pixel_website_trackers(),
+            pixels_koala.get_pixel_website_trackers(),
             pixels_posthog.get_pixel_website_trackers(),
             pixels_clearbit.get_pixel_website_trackers(),
-            pixels_instantly.get_pixel_website_trackers(),
+            # pixels_instantly.get_pixel_website_trackers(),
         ),
     )
