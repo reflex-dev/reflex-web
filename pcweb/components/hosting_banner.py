588328deba02040f972a15c233c876c6b4c4b80e
Alek Petuskey <alekpetuskey@Aleks-MacBook-Pro.local>
2024-11-08 17:17:55 -0800

    More updates

--- pcweb/components/hosting_banner.py
+++ pcweb/components/hosting_banner.py
@@ -5,30 +5,44 @@ def glow() -> rx.Component:
     return rx.box(
         class_name="absolute w-[120rem] h-[23.75rem] flex-shrink-0 rounded-[120rem] left-1/2 -translate-x-1/2 z-[0] top-[-16rem]",
         background_image=rx.color_mode_cond(
-            "radial-gradient(50% 50% at 50% 50%, rgba(235, 228, 255, 0.80) 0%, rgba(252, 252, 253, 0.00) 100%)",
-            "radial-gradient(50% 50% at 50% 50%, rgba(38, 25, 88, 1) 0%, rgba(21, 22, 24, 0.00) 100%)",
+            "radial-gradient(50% 50% at 50% 50%, rgba(235, 228, 255, 0.95) 0%, rgba(252, 252, 253, 0.00) 100%)",
+            "radial-gradient(50% 50% at 50% 50%, rgba(58, 45, 118, 1) 0%, rgba(21, 22, 24, 0.00) 100%)",
         ),
     )
 
 
+class HostingBannerState(rx.State):
+    show_banner: bool = True
+
+    def hide_banner(self):
+        print("hide_banner")
+        self.show_banner = False
+
 def hosting_banner() -> rx.Component:
-    return rx.box(
-        rx.text(
-            "Meet Hosting. ",
-            rx.el.span(
-                "Deploy your app with a single command. Performant, secure, and scalable.",
-                class_name="text-slate-12 font-medium text-sm",
+    return rx.cond(
+        HostingBannerState.show_banner,
+        rx.desktop_only(rx.box(
+            rx.link(
+                rx.box(
+                    rx.text(""),
+                    rx.text(
+                        "Reflex Cloud - ",
+                        rx.el.span(
+                            "Fast, secure & scalable hosting. One command to deploy.",
+                            class_name="text-slate-12 font-medium text-sm",
+                        ),
+                        class_name="text-slate-12 font-semibold text-sm z-[1]",
+                    ),
+                    rx.el.button(
+                        "Launching Soon",
+                        class_name="text-slate-11 h-[1.5rem] rounded-md bg-slate-4 px-1.5 text-sm font-semibold z-[1]",
+                    ),
+                    class_name="flex items-center gap-4",
+                ),
+                href="/hosting",
             ),
-            class_name="text-slate-12 font-semibold text-sm z-[1]",
-        ),
-        rx.link(
-            rx.el.button(
-                "Learn More",
-                class_name="text-slate-12 h-[1.75rem] rounded-md bg-slate-4 px-1.5 text-sm font-semibold",
-            ),
-            href="/hosting",
-            class_name="z-[1] shrink-0",
-        ),
-        glow(),
-        class_name="px-4 lg:px-6 w-screen h-auto lg:h-[65px] shadow-[inset_0_-1px_0_0_var(--c-slate-3)] flex items-center justify-center bg-slate-1 flex-row gap-4 overflow-hidden relative lg:py-0 py-2 max-w-full",
+            rx.icon("x", on_click=HostingBannerState.hide_banner, size=16, color=rx.color("slate", 9), class_name="absolute right-4 z-[1]"),
+            glow(),
+            class_name="px-4 lg:px-6 w-screen h-auto lg:h-[40px] shadow-[inset_0_-1px_0_0_var(--c-slate-3)] flex items-center justify-center bg-slate-1 flex-row gap-4 overflow-hidden relative lg:py-0 py-2 max-w-full",
+        ))
     )
--- pcweb/pages/hosting_countdown/waitlist.py
+++ pcweb/pages/hosting_countdown/waitlist.py
@@ -7,8 +7,14 @@ from pcweb.constants import REFLEX_DEV_WEB_PRICING_FORM_PRO_PLAN_WAITLIST_WEBHOO
 
 class WaitlistState(rx.State):
 
+    loading = False
+    success = False
+    
     @rx.event
     async def submit_pro_waitlist(self, form_data: dict):
+        self.loading = True
+        yield
+
         try:
             with httpx.Client() as client:
                 response: Response = client.post(
@@ -17,17 +23,20 @@ class WaitlistState(rx.State):
                 )
                 response.raise_for_status()
 
+            self.success = True
             yield rx.toast.success("Thank you for joining the waitlist!")
 
         except httpx.HTTPError:
             yield rx.toast.error("Failed to submit request. Please try again later.")
+
+        self.loading = False
 
 
 def waitlist():
     return rx.box(
         rx.box(
             rx.el.h2(
-                "Meet Reflex Hosting",
+                "Claim your spot!",
                 class_name="lg:text-5xl text-3xl gradient-heading font-semibold leading-[3.5rem] mb-4",
             ),
             rx.text(
@@ -36,21 +45,34 @@ def waitlist():
                 class_name="text-slate-9 lg:text-lg text-base mb-8 whitespace-pre-line font-medium text-center",
             ),
             rx.form(
-                rx.el.input(
-                    placeholder="Email",
-                    name="email",
-                    type="email",
-                    required=True,
-                    class_name="box-border flex flex-row gap-2 border-slate-5 bg-slate-1 focus:shadow-[0px_0px_0px_2px_var(--c-violet-4)] px-3.5 border rounded-[0.875rem] h-[3rem] font-medium text-slate-12 placeholder:text-slate-9 outline-none focus:outline-none caret-slate-12 max-w-[17.4rem] text-base",
-                ),
-                rx.el.button(
-                    "Join Waitlist",
-                    background="linear-gradient(180deg, #6E56CF 0%, #654DC4 100%)",
-                    _hover={
-                        "background": "linear-gradient(180deg, #6E56CF 0%, #6E56CF 100%)"
-                    },
-                    class_name="w-fit h-[3rem] px-3.5 rounded-[0.875rem] cursor-pointer text-[#FCFCFD] font-medium text-base transition-bg",
-                ),
+                rx.cond(
+                    WaitlistState.success,
+                    rx.badge("Thank you for joining the waitlist!", color_scheme="green", size="5"),
+                    rx.vstack(
+                        rx.hstack(
+                    rx.el.input(
+                        placeholder="Email",
+                        name="email",
+                        type="email",
+                        required=True,
+                        class_name="box-border flex flex-row gap-2 border-slate-5 bg-slate-1 focus:shadow-[0px_0px_0px_2px_var(--c-violet-4)] px-3.5 border rounded-[8px] h-[2rem] font-medium text-slate-12 placeholder:text-slate-9 outline-none focus:outline-none caret-slate-12 max-w-[17.4rem] text-base",
+                    ),
+                    rx.button(
+                        "Join Waitlist",
+                        background="linear-gradient(180deg, #6E56CF 0%, #654DC4 100%)",
+                        _hover={
+                            "background": "linear-gradient(180deg, #6E56CF 0%, #6E56CF 100%)"
+                        },
+                        class_name="w-fit h-[2rem] px-3.5 rounded-[8px] cursor-pointer text-[#FCFCFD] font-medium text-base transition-bg",
+                        ),
+                        is_loading=WaitlistState.loading,
+                    ),
+                    rx.text(
+                        "Join the waitlist to get access.",
+                        class_name="text-slate-8 lg:text-md text-base mb-8 whitespace-pre-line font-medium text-center",
+                    ), 
+                    align_items="center",
+                )),   
                 on_submit=WaitlistState.submit_pro_waitlist,
                 class_name="flex lg:flex-row flex-col gap-2 justify-center items-center",
             ),
