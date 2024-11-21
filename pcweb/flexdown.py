6a2c62ff474acae40969ac26d2395beccda68d02
Elijah Ahianyo <elijahahianyo@gmail.com>
2024-11-21 05:16:42 +0000

    More shiki cleanup (#1093)
    * More shiki cleanup
    
    * intro page
    
    * use a shiki markdown based wrapper
    
    * fix ref
    
    * minor refactor

--- pcweb/flexdown.py
+++ pcweb/flexdown.py
@@ -111,7 +111,7 @@ class AlertBlock(flexdown.blocks.MarkdownBlock):
                                 color=f"{rx.color(color, 11)}",
                             ),
                             (
-                                rx.markdown(
+                                markdown_with_shiki(
                                     title, margin_y="0px", style=get_code_style(color)
                                 )
                                 if title
@@ -171,7 +171,7 @@ class AlertBlock(flexdown.blocks.MarkdownBlock):
                         ),
                         color=f"{rx.color(color, 11)}",
                     ),
-                    rx.markdown(
+                    markdown_with_shiki(
                         title,
                         color=f"{rx.color(color, 11)}",
                         margin_y="0px",
@@ -369,13 +369,13 @@ class VideoBlock(flexdown.blocks.MarkdownBlock):
                 rc.accordion_button(
                     rx.hstack(
                         (
-                            rx.markdown(
+                            markdown_with_shiki(
                                 title,
                                 margin_y="0px",
                                 style=get_code_style(color),
                             )
                             if title
-                            else rx.markdown("Video Description")
+                            else markdown_with_shiki("Video Description")
                         ),
                         rx.spacer(),
                         rc.accordion_icon(color=f"{rx.color(color, 11)}"),
@@ -534,3 +534,21 @@ xd2.clear_modules()
 
 def markdown(text):
     return xd.get_default_block().render_fn(content=text)
+
+
+def markdown_with_shiki(*args, **kwargs):
+    """
+    Wrapper for the markdown component with a customized component map.
+    Uses the experimental Shiki-based code block (rx._x.code_block)
+    instead of the default CodeBlock component for code blocks.
+
+    Note: This wrapper should be removed once the default codeblock
+    in rx.markdown component map is updated to the Shiki-based code block.
+    """
+    return rx.markdown(
+        *args,
+        component_map={
+            "codeblock": lambda value, **props: rx._x.code_block(value, **props)
+        },
+        **kwargs
+    )
