e6a9a375fc0d3f0af51796c0c39ffd96f96ab6bc
Masen Furer <m_github@0x26.net>
2024-11-25 13:34:30 -0800

    add model_editor page to sidebar

--- pcweb/components/docpage/sidebar/sidebar_items/learn.py
+++ pcweb/components/docpage/sidebar/sidebar_items/learn.py
@@ -166,6 +166,7 @@ def get_sidebar_items_backend():
                 database.tables,
                 database.queries,
                 database.relationships,
+                database.model_editor,
             ],
         ),
         create_item(
