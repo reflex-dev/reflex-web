0aeee890eb155d9d4a1222acb313534d7e88a069
Nikhil Rao <nikhil@reflex.dev>
2024-12-04 13:11:32 -0800

    Add new pixel (#1127)
    * Add new pixel
    
    * remove unused pixels
    
    ---------
    
    Co-authored-by: Tom Gotsman <tomgotsman@Toms-MacBook-Pro.local>

--- pcweb/telemetry/pixels.py
+++ pcweb/telemetry/pixels.py
@@ -14,11 +14,9 @@ def get_pixel_website_trackers() -> list[rx.Component]:
     return list(
         itertools.chain(
             pixels_google.get_pixel_website_trackers(),
-            # pixels_common_room.get_pixel_website_trackers(),
             pixels_koala.get_pixel_website_trackers(),
             pixels_posthog.get_pixel_website_trackers(),
             pixels_clearbit.get_pixel_website_trackers(),
             pixels_rb2b.get_pixel_rb2b_website_trackers(),
-            # pixels_instantly.get_pixel_website_trackers(),
         ),
     )
