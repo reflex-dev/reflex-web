7840c82323d3f9b7dcf98732ea60262809f2558d
Tom Gotsman <64492814+tgberkeley@users.noreply.github.com>
2025-01-22 15:27:04 -0800

    remove posthog pixel (#1183)
    * remove posthog pixel
    
    * remove clearbit pixel
    
    ---------
    
    Co-authored-by: Tom Gotsman <tomgotsman@Toms-MacBook-Pro.local>

--- pcweb/telemetry/pixels.py
+++ pcweb/telemetry/pixels.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import itertools
 from typing import TYPE_CHECKING
 
-from pcweb.telemetry import pixels_clearbit, pixels_google, pixels_koala, pixels_posthog, pixels_rb2b
+from pcweb.telemetry import pixels_google, pixels_koala, pixels_rb2b
 
 if TYPE_CHECKING:
     import reflex as rx
@@ -15,8 +15,6 @@ def get_pixel_website_trackers() -> list[rx.Component]:
         itertools.chain(
             pixels_google.get_pixel_website_trackers(),
             pixels_koala.get_pixel_website_trackers(),
-            pixels_posthog.get_pixel_website_trackers(),
-            pixels_clearbit.get_pixel_website_trackers(),
             pixels_rb2b.get_pixel_rb2b_website_trackers(),
         ),
     )
