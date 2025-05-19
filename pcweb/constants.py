284b2b0cab3169714acad6681ff6ac5121d08ecc
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-05-19 11:50:50 -0700

    add env flags

--- pcweb/constants.py
+++ pcweb/constants.py
@@ -17,8 +17,8 @@ REFLEX_DEV_WEB_GENERAL_FORM_FEEDBACK_WEBHOOK_URL: str = os.environ.get(
 # pcweb urls.
 REFLEX_URL = "https://reflex.dev/"
 REFLEX_DOCS_URL = "https://reflex.dev/docs/getting-started/introduction/"
-REFLEX_CLOUD_URL = "https://cloud.reflex.dev/"
-REFLEX_BUILD_URL = "https://build.reflex.dev/"
+REFLEX_CLOUD_URL = os.getenv("REFLEX_CLOUD_URL", "https://cloud.reflex.dev/")
+REFLEX_BUILD_URL = os.getenv("REFLEX_BUILD_URL", "https://build.reflex.dev/")
 PYNECONE_URL = "https://pynecone.io"
 PIP_URL = "https://pypi.org/project/reflex"
 GITHUB_URL = "https://github.com/reflex-dev/reflex"
