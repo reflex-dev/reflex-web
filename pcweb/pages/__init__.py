ff2f5e15c529ca2b497120201ee1976f89a65bd2
Ahmad Hakim <84860195+LineIndent@users.noreply.github.com>
2025-06-11 00:52:49 +0300

    ENG-6205: databricks docs (#1420)
    * databricks docs
    
    * more databricks
    
    * db + reflex
    
    * fixes
    
    * fixes
    
    * remove unsued images

--- pcweb/pages/__init__.py
+++ pcweb/pages/__init__.py
@@ -1,29 +1,21 @@
 from pcweb.route import Route
 from .affiliates import affiliates as affiliates
+from .databricks import databricks as databricks
 from .blog import blog_routes
 from .customers.data.customers import customers_routes
-from .customers.data.customers import customers_routes
-from .customers.landing import customers as customers
 from .customers.landing import customers as customers
 from .docs import doc_routes
-from .errors import errors as errors
 from .errors import errors as errors
 from .faq import faq as faq
 from .framework.framework import framework as framework
 from .gallery import gallery as gallery
-from .gallery import gallery as gallery
 from .gallery.apps import gallery_apps_routes
-from .gallery.apps import gallery_apps_routes
-from .hosting.hosting import hosting_landing as hosting_landing
 from .hosting.hosting import hosting_landing as hosting_landing
 from .landing.landing import landing as landing
-from .page404 import page404 as page404
 from .page404 import page404 as page404
 from .pricing.pricing import pricing as pricing
 
 # from .hosting_countdown.hosting_countdown import hosting_countdown as hosting_countdown
-from .pricing.pricing import pricing as pricing
-from .sales import sales as sales
 from .sales import sales as sales
 
 routes = [
---
+++ pcweb/pages/databricks/databricks.py
@@ -0,0 +1,35 @@
+
+import reflex as rx
+
+import flexdown
+from pcweb.flexdown import xd2 as xd
+from pcweb.components.docpage.navbar import navbar
+from pcweb.pages.customers.views.footer import footer_customer
+from pcweb.views.bottom_section.bottom_section import bottom_section
+from pcweb.pages.framework.index_colors import index_colors
+
+
+document = flexdown.parse_file("pcweb/pages/databricks/databricks.md")
+
+def databricks_content():
+    return rx.box(xd.render(document, document.filename))
+
+
+@rx.page(route="/databricks", title="Databricks - Reflex")
+def databricks():
+    return rx.box(
+        rx.box(
+            index_colors(),
+            navbar(),
+            rx.el.main(
+                databricks_content(),
+                rx.box(class_name="flex-grow"),
+                class_name="w-full z-[1] relative flex flex-col justify-center mx-auto max-w-[640px] lg:px-0 px-4 pb-20",
+            ),
+            rx.box(class_name="h-[1px] bg-slate-3 w-full"),
+            bottom_section(),
+            footer_customer(),
+            class_name="relative flex flex-col justify-start items-center w-full h-full min-h-screen font-instrument-sans gap-4 mx-auto max-w-[64.19rem] lg:border-x border-slate-3 pt-24 lg:pt-48",
+        ),
+        class_name="relative overflow-hidden",
+    )
