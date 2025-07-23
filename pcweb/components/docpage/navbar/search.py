492b6608d8f9c5b6d7ee0f62361299a1b80c0c4f
carlosabadia <cutillascarlos@gmail.com>
2025-07-23 21:51:18 +0200

    revert to inkeep

--- pcweb/components/docpage/navbar/search.py
+++ pcweb/components/docpage/navbar/search.py
@@ -1,9 +1,9 @@
 """Search bar component for the navbar."""
 
 import reflex as rx
-from .typesense import typesense_search
+from .inkeep import inkeep
 
 
 @rx.memo
 def search_bar() -> rx.Component:
-    return typesense_search()
+    return inkeep()
