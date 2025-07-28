58d275a56a218011bf7153652e7d330a73c9e64a
carlosabadia <cutillascarlos@gmail.com>
2025-07-28 17:12:02 +0200

    remove custom reflex badge

--- pcweb/pages/affiliates/affiliates.py
+++ pcweb/pages/affiliates/affiliates.py
@@ -196,6 +196,5 @@ def affiliates() -> rx.Component:
             class_name="section-content",
         ),
         footer_index(),
-        badge(),
         class_name="flex flex-col w-full max-w-[94.5rem] justify-center items-center mx-auto px-4 lg:px-5 relative overflow-hidden",
     )
