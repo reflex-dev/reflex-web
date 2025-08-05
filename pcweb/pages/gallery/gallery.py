f9f447879c033601534afd57e2a559005045d203
carlosabadia <cutillascarlos@gmail.com>
2025-08-05 11:38:11 +0200

    show loading r logo until template iframe is loaded

--- pcweb/pages/gallery/gallery.py
+++ pcweb/pages/gallery/gallery.py
@@ -4,9 +4,11 @@ import re
 from pcweb.templates.webpage import webpage
 from pcweb.components.button import button
 from pcweb.components.icons import get_icon
+from pcweb.components.r_svg_loader import r_svg_loader
 
 REFLEX_BUILD_TEMPLATES_PATH = "reflex_build_templates/"
 REFLEX_BUILD_TEMPLATES_IMAGES = "reflex_build_template_images/"
+
 
 def get_templatey_apps(paths):
     """Method to parse each markdown file and return the data from the file"""
@@ -16,6 +18,7 @@ def get_templatey_apps(paths):
         key = str(path).replace(".md", "/")
         gallery_apps[key] = document
     return gallery_apps
+
 
 paths = flexdown.utils.get_flexdown_files(REFLEX_BUILD_TEMPLATES_PATH)
 template_apps_data = get_templatey_apps(paths)
@@ -33,6 +36,10 @@ def app_dialog_with_trigger(
         rx.dialog.trigger(trigger_content, class_name="w-full h-full"),
         rx.dialog.content(
             rx.el.div(
+                rx.el.div(
+                    r_svg_loader(),
+                    class_name="absolute inset-0 flex items-center justify-center",
+                ),
                 rx.el.div(
                     rx.el.div(
                         rx.el.p(
@@ -54,16 +61,24 @@ def app_dialog_with_trigger(
                 ),
                 rx.el.iframe(
                     src=app_url,
-                    class_name="w-full h-full xl:rounded-md shadow-small",
+                    class_name="w-full h-full xl:rounded-md shadow-small z-10",
                     id="iFrame",
                 ),
-                class_name="flex flex-col w-full h-full gap-y-3",
+                class_name="flex flex-col w-full h-full gap-y-3 relative",
             ),
             class_name="w-full !max-w-[90em] xl:max-w-[110em] 2xl:max-w-[120em] h-[80vh]",
         ),
     )
 
-def extended_gallery_grid_item(app_url: str, app_name: str, app_author: str, app_thread: str, app_image: str, app_inner_page: str):
+
+def extended_gallery_grid_item(
+    app_url: str,
+    app_name: str,
+    app_author: str,
+    app_thread: str,
+    app_image: str,
+    app_inner_page: str,
+):
     return app_dialog_with_trigger(
         app_url=app_url,
         app_author=app_author,
@@ -90,7 +105,9 @@ def extended_gallery_grid_item(app_url: str, app_name: str, app_author: str, app
                             class_name="no-underline flex-1",
                             on_click=rx.stop_propagation,
                         ),
-                        button("Preview", variant="primary", size="md", class_name="flex-1"),
+                        button(
+                            "Preview", variant="primary", size="md", class_name="flex-1"
+                        ),
                         class_name="flex flex-row gap-x-2 w-full items-stretch px-4 pb-4",
                     ),
                     class_name="absolute inset-0 flex items-end justify-center z-20 opacity-0 translate-y-2 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300 ease-out pointer-events-none group-hover:pointer-events-auto z-[99]",
@@ -99,10 +116,16 @@ def extended_gallery_grid_item(app_url: str, app_name: str, app_author: str, app
             ),
             rx.el.div(
                 rx.el.div(
-                    get_icon(icon="badge_logo", class_name="size-5 flex-shrink-0",),
+                    get_icon(
+                        icon="badge_logo",
+                        class_name="size-5 flex-shrink-0",
+                    ),
                     rx.el.div(
                         rx.el.p(app_name, class_name="text-sm font-medium truncate"),
-                        rx.el.p(app_author, class_name="text-sm font-regular text-slate-9 truncate"),
+                        rx.el.p(
+                            app_author,
+                            class_name="text-sm font-regular text-slate-9 truncate",
+                        ),
                         class_name="flex flex-col gap-y-0 min-w-0 w-full",
                     ),
                     class_name="flex flex-row gap-x-2 items-start min-w-0 w-full",
@@ -122,23 +145,26 @@ def create_grid_with_items():
         app_name = meta.get("title", "Untitled").replace("_", " ").title()
         app_author = meta.get("author", "Team Reflex")
         app_thread = f"/gen/{app_name.lower().replace(' ', '-')}/"
-        app_image = meta.get('image', "")
+        app_image = meta.get("image", "")
         slug = re.sub(r"[\s_]+", "-", meta.get("title", "")).lower()
         app_inner_page = f"/templates/{slug}"
 
-        items.append(extended_gallery_grid_item(
-            app_url=app_url,
-            app_name=app_name,
-            app_author=app_author,
-            app_thread=app_thread,
-            app_image=f"/{REFLEX_BUILD_TEMPLATES_IMAGES}{app_image}",
-            app_inner_page=app_inner_page
-        ))
+        items.append(
+            extended_gallery_grid_item(
+                app_url=app_url,
+                app_name=app_name,
+                app_author=app_author,
+                app_thread=app_thread,
+                app_image=f"/{REFLEX_BUILD_TEMPLATES_IMAGES}{app_image}",
+                app_inner_page=app_inner_page,
+            )
+        )
 
     return rx.el.div(
         *items,
-        class_name="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 md:px-8 lg:px-8"
+        class_name="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 md:px-8 lg:px-8",
     )
+
 
 def create_header():
     return rx.box(
@@ -155,6 +181,7 @@ def create_header():
         ),
         class_name="flex flex-col justify-center items-center gap-6 w-full text-center",
     )
+
 
 @webpage(path="/templates", title="Templates · Reflex")
 def gallery() -> rx.Component:
