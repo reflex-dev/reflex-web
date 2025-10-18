13e6d67493cd24df5f0dda28405afbf4fcd38854
Tom Gotsman <tomgotsman@Toms-MacBook-Pro-4.local>
2025-10-17 19:55:58 -0700

    trailing space

--- pcweb/pages/integrations/integration_header.py
+++ pcweb/pages/integrations/integration_header.py
@@ -8,7 +8,7 @@ def integration_header() -> rx.Component:
             class_name="max-w-full inline-block bg-clip-text bg-gradient-to-r from-slate-12 to-slate-11 w-full font-xx-large text-center text-transparent text-balance mx-auto break-words",
         ),
         rx.el.h2(
-            """Easily connect with the tools your team already uses 
+            """Easily connect with the tools your team already uses
     or extend your app with any Python SDK, library, or API.""",
             class_name="max-w-full w-full font-semibold text-md text-center text-slate-11 -mt-2 md:text-2xl mx-auto text-balance word-wrap break-words md:whitespace-pre",
         ),
