757f765f55b849c5383f43be0d4c908d4d9f5729
Alek Petuskey <alekpetuskey@gmail.com>
2026-01-09 18:18:52 -0800

    Apply ruff formatting

--- pcweb/pages/meeting_successfully_booked.py
+++ pcweb/pages/meeting_successfully_booked.py
@@ -19,9 +19,7 @@ def page_meeting_successfully_booked():
             class_name="font-md text-balance text-slate-9 text-center mt-4",
         ),
         rx.box(
-            ui.button(
-                "Home", variant="primary", size="lg", on_click=rx.redirect("/")
-            ),
+            ui.button("Home", variant="primary", size="lg", on_click=rx.redirect("/")),
             class_name="flex flex-row items-center gap-x-4 mt-8",
         ),
         class_name="h-[60vh] w-full flex flex-col items-center justify-center p-4",
