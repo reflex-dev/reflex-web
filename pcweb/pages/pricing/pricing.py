e2361fba6d0ee5d97d4c9347473bc96d0c1fe826
Carlos <cutillascarlos@gmail.com>
2026-01-26 12:29:14 +0100

    ENG-8726: Remove pricing slider

--- pcweb/pages/pricing/pricing.py
+++ pcweb/pages/pricing/pricing.py
@@ -6,7 +6,7 @@ from pcweb.pages.framework.views.footer_index import footer_index
 from pcweb.pages.pricing.calculator import calculator_section
 from pcweb.pages.pricing.faq import faq
 from pcweb.pages.pricing.plan_cards import plan_cards
-from pcweb.pages.pricing.slider_calculator import MachineState, slider_calculator
+from pcweb.pages.pricing.slider_calculator import MachineState
 from pcweb.pages.pricing.table import tiers_tables
 
 pricing_path = "/pricing"
@@ -29,7 +29,6 @@ def pricing() -> rx.Component:
             rx.box(
                 plan_cards(),
                 tiers_tables(),
-                slider_calculator(),
                 calculator_section(),
                 faq(),
                 class_name="flex flex-col relative justify-center items-center w-full",
