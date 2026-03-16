f1fbf2f85b1a2a97c1a95d4efc9e8263fe242629
Carlos <cutillascarlos@gmail.com>
2026-03-16 17:05:28 +0100

    add integrations

--- pcweb/pages/templates/template_details/views/sidebar.py
+++ pcweb/pages/templates/template_details/views/sidebar.py
@@ -3,7 +3,14 @@ import reflex_ui as ui
 
 from pcweb.components.hosting_banner import HostingBannerState
 from pcweb.components.marketing_button import button as marketing_button
+from pcweb.constants import INTEGRATIONS_IMAGES_URL
 from pcweb.pages.templates.templates_state import TemplatesState
+
+SECTION_HEADER_CLASS = (
+    "font-mono font-[415] text-[0.75rem] leading-4 uppercase pb-4",
+    "border-b border-dashed dark:border-m-slate-8 border-m-slate-6",
+    "dark:text-m-slate-6 text-m-slate-7",
+)
 
 
 def links_section() -> rx.Component:
@@ -41,10 +48,7 @@ def links_section() -> rx.Component:
 
 def quick_start() -> rx.Component:
     return rx.el.div(
-        rx.el.span(
-            "Quick Start",
-            class_name="font-mono font-[415] text-[0.75rem] leading-4 uppercase pb-4 border-b border-dashed dark:border-m-slate-8 border-m-slate-6 dark:text-m-slate-6 text-m-slate-7",
-        ),
+        rx.el.span("Quick Start", class_name=SECTION_HEADER_CLASS),
         rx.el.div(
             rx.el.div(
                 rx.el.span("# Install reflex", class_name="text-[#ACB2BE]"),
@@ -72,10 +76,7 @@ def quick_start() -> rx.Component:
 
 def tech_stack() -> rx.Component:
     return rx.el.div(
-        rx.el.span(
-            "Tech Stack",
-            class_name="font-mono font-[415] text-[0.75rem] leading-4 uppercase pb-4 border-b border-dashed dark:border-m-slate-8 border-m-slate-6 dark:text-m-slate-6 text-m-slate-7",
-        ),
+        rx.el.span("Tech Stack", class_name=SECTION_HEADER_CLASS),
         rx.el.div(
             rx.foreach(
                 TemplatesState.active_template.tech_stack,
@@ -92,10 +93,7 @@ def tech_stack() -> rx.Component:
 
 def requirements() -> rx.Component:
     return rx.el.div(
-        rx.el.span(
-            "Requirements",
-            class_name="font-mono font-[415] text-[0.75rem] leading-4 uppercase pb-4 border-b border-dashed dark:border-m-slate-8 border-m-slate-6 dark:text-m-slate-6 text-m-slate-7",
-        ),
+        rx.el.span("Requirements", class_name=SECTION_HEADER_CLASS),
         rx.el.div(
             rx.foreach(
                 TemplatesState.active_template.requirements,
@@ -116,17 +114,14 @@ def requirements() -> rx.Component:
 
 def key_features() -> rx.Component:
     return rx.el.div(
-        rx.el.span(
-            "Key Features",
-            class_name="font-mono font-[415] text-[0.75rem] leading-4 uppercase pb-4 border-b border-dashed dark:border-m-slate-8 border-m-slate-6 dark:text-m-slate-6 text-m-slate-7",
-        ),
+        rx.el.span("Key Features", class_name=SECTION_HEADER_CLASS),
         rx.el.div(
             rx.foreach(
                 TemplatesState.active_template.key_features,
-                lambda requirement: rx.el.div(
+                lambda feature: rx.el.div(
                     ui.icon("Tick02Icon", class_name="text-primary-10 mt-1"),
                     rx.el.span(
-                        requirement,
+                        feature,
                         class_name="text-sm font-medium text-secondary-12",
                     ),
                     class_name="flex items-start gap-2.5",
@@ -138,11 +133,75 @@ def key_features() -> rx.Component:
     )
 
 
+def enabled_integrations() -> rx.Component:
+    return rx.el.div(
+        rx.el.span("Integrations", class_name=SECTION_HEADER_CLASS),
+        rx.el.div(
+            integrations_stack(
+                integrations=TemplatesState.active_template.enabled_integrations
+            ),
+            class_name="flex flex-col gap-6",
+        ),
+        class_name="flex flex-col gap-6",
+    )
+
+
+def enabled_integration_icon(integration_name: str) -> rx.Component:
+    return ui.avatar.root(
+        ui.avatar.image(
+            src=rx.color_mode_cond(
+                f"{INTEGRATIONS_IMAGES_URL}light/{integration_name}.svg",
+                f"{INTEGRATIONS_IMAGES_URL}dark/{integration_name}.svg",
+            ),
+            unstyled=True,
+            class_name="size-4",
+        ),
+        ui.avatar.fallback(
+            integration_name[0],
+            class_name="text-secondary-12 text-base font-semibold uppercase size-4 flex justify-center items-center",
+            unstyled=True,
+        ),
+        unstyled=True,
+        class_name="size-4 flex items-center justify-center shrink-0",
+    )
+
+
+def integrations_stack(
+    integrations: list[str],
+) -> rx.Component:
+    return rx.el.div(
+        rx.foreach(
+            integrations,
+            lambda integration: integrations_circle(integration_name=integration),
+        ),
+        class_name="flex flex-row -space-x-2 flex-wrap gap-y-2",
+    )
+
+
+@rx.memo
+def integrations_circle(integration_name: str) -> rx.Component:
+    formatted_integration_name = integration_name.lower().replace("_", " ")
+    return rx.el.div(
+        ui.tooltip(
+            trigger=rx.el.div(
+                enabled_integration_icon(integration_name=formatted_integration_name),
+                class_name="flex size-8 shrink-0 justify-center items-center rounded-full shadow-small border border-secondary-a5 bg-white-1 dark:bg-secondary-1 cursor-default",
+            ),
+            side="bottom",
+            content=integration_name,
+        ),
+        class_name="z-[1]",
+    )
+
+
 def sidebar_content() -> rx.Component:
     return rx.fragment(
         links_section(),
         quick_start(),
         rx.cond(TemplatesState.active_template.tech_stack, tech_stack()),
+        rx.cond(
+            TemplatesState.active_template.enabled_integrations, enabled_integrations()
+        ),
         rx.cond(TemplatesState.active_template.key_features, key_features()),
         rx.cond(TemplatesState.active_template.requirements, requirements()),
     )
@@ -155,7 +214,7 @@ def sidebar_mobile() -> rx.Component:
     )
 
 
-def sidebar():
+def sidebar() -> rx.Component:
     return rx.el.div(
         rx.el.div(
             sidebar_content(),
