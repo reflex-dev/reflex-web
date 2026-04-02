cb0b4232a3d3daa4ff396617a850925aaad54fb3
carlosabadia <cutillascarlos@gmail.com>
2026-04-02 11:12:00 +0200

    remove united health group logo

--- pcweb/pages/use_cases/common/features_2.py
+++ pcweb/pages/use_cases/common/features_2.py
@@ -21,17 +21,17 @@ def feature_card(
         rx.el.span(title, class_name="font-semibold text-slate-12 text-lg mt-4"),
         rx.el.p(
             description,
-            class_name="text-m-slate-11 dark:text-m-slate-9 text-sm font-medium mt-2",
+            class_name="text-secondary-11 text-sm font-medium mt-2",
         ),
         rx.el.ul(
             *[
                 rx.el.li(
                     item,
-                    class_name="text-m-slate-11 dark:text-m-slate-9 text-sm font-medium mt-1",
+                    class_name="text-secondary-11 text-sm font-medium mt-1",
                 )
                 for item in items
             ],
-            class_name="text-m-slate-11 dark:text-m-slate-9 text-sm font-medium list-disc list-inside mt-2",
+            class_name="text-secondary-11 text-sm font-medium list-disc list-inside mt-2",
         )
         if items
         else None,
